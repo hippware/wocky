cc55d4328e43804b3f2cc863734352e0f1c892ff
Phil Toland <phil@hippware.com>
2015-11-13 12:17:13 -0600

    Stub out all of the behavior functions for gen_auth

--- apps/wocky/src/ejabberd_auth_cassandra.erl
+++ apps/wocky/src/ejabberd_auth_cassandra.erl
@@ -8,10 +8,17 @@
 %%%
 %%% Complications arise because they are not identical.
 %%%
-%%% Ejabberd users are a (localpart, domainpart) pair with localpart being the 'username'.
-%%% Wocky users are a (localpart, domainpart, username) tuple with username being a separate quantity (which is also globally unique across all domains).
+%%% Ejabberd users are a (localpart, domainpart) pair with localpart being the
+%%% 'username'. Wocky users are a (localpart, domainpart, username) tuple with
+%%% username being a separate quantity (which is also globally unique across all
+%%% domains).
 %%%
-%%% In order to utilise existing code, this module needs to conform to {@link ejabberd_gen_auth} but not all of the functions required of (1) make sense for (2). Hence, for those functions which don't make sense, a "best effort" implementation which is "least surprising" will have to suffice. In other words, all the functions of (1) need to be implemented, but not all of them will be useful or are expected to be used in normal operations.
+%%% In order to utilise existing code, this module needs to conform to {@link
+%%% ejabberd_gen_auth} but not all of the functions required of (1) make sense
+%%% for (2). Hence, for those functions which don't make sense, a "best effort"
+%%% implementation which is "least surprising" will have to suffice. In other
+%%% words, all the functions of (1) need to be implemented, but not all of them
+%%% will be useful or are expected to be used in normal operations.
 %%%
 %%%
 %%% For schema, see priv/schema*.cql
@@ -24,17 +31,30 @@
 
 -module(ejabberd_auth_cassandra).
 
-% -behaviour(ejabberd_gen_auth).
+-behaviour(ejabberd_gen_auth).
 -export([start/1,
          stop/1,
+         store_type/1,
+         login/2,
+         set_password/3,
+         check_password/3,
+         check_password/5,
          try_register/3,
+         dirty_get_registered_users/0,
+         get_vh_registered_users/1,
+         get_vh_registered_users/2,
          get_vh_registered_users_number/1,
          get_vh_registered_users_number/2,
-         is_user_exists/2,
-         remove_user/3
-         ]).
+         get_password/2,
+         get_password_s/2,
+         get_password/3,
+         does_user_exist/2,
+         remove_user/2,
+         remove_user/3,
+         plain_password_required/0]).
 
 -include_lib("ejabberd/include/ejabberd.hrl").
+
 
 %%%----------------------------------------------------------------------
 %%% API
@@ -44,85 +64,165 @@
 start(_Host) ->
     ok.
 
+
 -spec stop(Host :: ejabberd:server()) -> ok.
 stop(_Host) ->
     ok.
 
-% This is invoked from ejabberd_auth:try_register().
-% Not really suitable for use since it does not pass in extra profile information.
-% Exists for completeness more than anything else (at the moment)
--spec try_register(User :: ejabberd:user(),
-                   Server :: ejabberd:server(),
+
+-spec store_type(Host :: ejabberd:lserver()) -> scram | plain | external.
+store_type(_Host) ->
+    scram.
+
+
+-spec login(User :: ejabberd:luser(),
+            Server :: ejabberd:lserver()) -> boolean().
+login(_User, _Server) ->
+    false.
+
+
+-spec set_password(User :: ejabberd:luser(),
+                   Server :: ejabberd:lserver(),
                    Password :: binary()
-                   ) -> {atomic, ok | exists}
-                      | {error, invalid_jid | not_allowed}
-                      | {aborted, _}.
+                  ) -> ok | {error, not_allowed | invalid_jid}.
+set_password(_User, _Server, _Password) ->
+    {error, not_allowed}.
+
+
+-spec check_password(User :: ejabberd:luser(),
+                     Server :: ejabberd:lserver(),
+                     Password :: binary()) -> boolean().
+check_password(_User, _Server, _Password) ->
+    false.
+
+
+-spec check_password(User :: ejabberd:luser(),
+                         Server :: ejabberd:lserver(),
+                         Password :: binary(),
+                         Digest :: binary(),
+                         DigestGen :: fun()) -> boolean().
+check_password(_User, _Server, _Password, _Digest, _DigestGen) ->
+    false.
+
+
+%% This is invoked from ejabberd_auth:try_register().
+%% Not really suitable for use since it does not pass in extra profile information.
+%% Exists for completeness more than anything else (at the moment)
+-spec try_register(User :: ejabberd:luser(),
+                   Server :: ejabberd:lserver(),
+                   Password :: binary()
+                  ) -> ok | {error, exists | not_allowed | term()}.
 try_register(User, Server, Password) ->
-    LUser = jlib:nodeprep(User),
-    LServer = jlib:nameprep(Server),
-    if
-        (LUser == error) or (LServer == error) ->
-            {error, invalid_jid};
+    Id = cassandra:timeuuid(Server),
+    {ok, Return} = cassandra:pquery(shared, <<"INSERT INTO username_to_user (username, id, domain) VALUES (?, ?, ?) IF NOT EXISTS">>, [User, Id, Server], quorum),
+    Result = cassandra:single_result(Return),
+    %% Note: Result is <<1>> for success, <<0>> if error.
+    %% There is no documentation on the return type so it's possible,
+    %%   in the future, this may not be a binary.
+    Success = Result /= <<0>>,
+
+    case Success of
         true ->
-            Id = cassandra:timeuuid(Server),
-            {ok, Return} = cassandra:pquery(shared, <<"INSERT INTO username_to_user (username, id, domain) VALUES (?, ?, ?) IF NOT EXISTS">>, [LUser, Id, LServer], quorum),
-            Result = cassandra:single_result(Return),
-            % Note: Result is <<1>> for success, <<0>> if error.
-            % There is no documentation on the return type so it's possible,
-            %   in the future, this may not be a binary.
-            Success = Result /= <<0>>,
+            Password2 = case scram:enabled(Server) and is_binary(Password) of
+                            true ->
+                                scram:password_to_scram(Password, scram:iterations(Server));
+                            false ->
+                                %% Store plaintext passwords inside scram fields with iteration = 0 (which is an invalid value)
+                                #scram{storedkey = Password,
+                                       serverkey = <<"">>,
+                                       salt = <<"">>,
+                                       iterationcount = 0}
+                        end,
 
-            case Success of
-                true ->
-                    Password2 = case scram:enabled(Server) and is_binary(Password) of
-						true ->
-                            scram:password_to_scram(Password, scram:iterations(Server));
-						false ->
-                            % Store plaintext passwords inside scram fields with iteration = 0 (which is an invalid value)
-                            #scram{storedkey = Password,
-                                serverkey = <<"">>,
-                                salt = <<"">>,
-                                iterationcount = 0}
-                    end,
+            {ok, _} = cassandra:pquery(Server, <<"INSERT INTO user (id, domain, username, stored_key, server_key, salt, iteration_count) VALUES (?, ?, ?, ?, ?, ?, ?)">>,
+                                       [Id, Server, User, Password2#scram.storedkey, Password2#scram.serverkey, Password2#scram.salt, Password2#scram.iterationcount], quorum),
+            ok;
 
-                    {ok, _} = cassandra:pquery(LServer, <<"INSERT INTO user (id, domain, username, stored_key, server_key, salt, iteration_count) VALUES (?, ?, ?, ?, ?, ?, ?)">>,
-                        [Id, LServer, LUser, Password2#scram.storedkey, Password2#scram.serverkey, Password2#scram.salt, Password2#scram.iterationcount], quorum),
-                    {atomic, ok};
-                false ->
-                    {atomic, exists}
-            end
+        false ->
+            {error, exists}
     end.
 
--spec get_vh_registered_users_number(Server :: ejabberd:server()
-                                    ) -> non_neg_integer().
+
+-spec dirty_get_registered_users() -> [ejabberd:simple_bare_jid()].
+dirty_get_registered_users() ->
+    erlang:error(not_implemented).
+
+
+-spec get_vh_registered_users(Server :: ejabberd:lserver()) -> [ejabberd:simple_bare_jid()].
+get_vh_registered_users(_Server) ->
+    erlang:error(not_implemented).
+    
+
+-spec get_vh_registered_users(Server :: ejabberd:lserver(),
+                              Opts :: list()
+                             ) -> [ejabberd:simple_bare_jid()].
+get_vh_registered_users(_Server, _Opts) ->
+    erlang:error(not_implemented).
+
+
+-spec get_vh_registered_users_number(Server :: ejabberd:lserver()) -> integer().
 get_vh_registered_users_number(_Server) ->
     % Let's not do a SELECT COUNT(*)
     -1.
 
-get_vh_registered_users_number(Server, _) ->
+
+-spec get_vh_registered_users_number(Server :: ejabberd:lserver(),
+                                     Opts :: list()) -> integer().
+get_vh_registered_users_number(Server, _Opts) ->
     get_vh_registered_users_number(Server).
 
--spec is_user_exists(User :: ejabberd:user(),
-                     Server :: ejabberd:server()
+
+-spec get_password(User :: ejabberd:luser(),
+                   Server :: ejabberd:lserver()
+                  ) -> scram:scram_tuple() | binary() | false.
+get_password(_User, _Server) ->
+    false.
+
+
+-spec get_password_s(User :: ejabberd:luser(),
+                     Server :: ejabberd:lserver()) -> binary().
+get_password_s(_User, _Server) ->
+    <<"">>.
+
+
+-spec get_password(User :: ejabberd:luser(),
+                   Server :: ejabberd:lserver(),
+                   DefaultValue :: binary()
+                  ) -> scram:scram_tuple() | binary() | false.
+get_password(_User, _Server, _DefaultValue) ->
+    false.
+
+
+-spec does_user_exist(User :: ejabberd:luser(),
+                      Server :: ejabberd:lserver()
                      ) -> boolean() | {error, atom()}.
-is_user_exists(User, _Server) ->
-    LUser = jlib:nodeprep(User),
-    {ok, Return} = cassandra:pquery(shared, <<"SELECT username, id, domain FROM username_to_user WHERE username = ?">>, [LUser], quorum),
+does_user_exist(User, _Server) ->
+    {ok, Return} = cassandra:pquery(shared, <<"SELECT username, id, domain FROM username_to_user WHERE username = ?">>, [User], quorum),
     Result = cassandra:rows(Return),
     length(Result) > 0.
 
-%% @doc Remove user if the provided password is correct.
-%% This functionality is unused (only called from mod_register) so just stub it out.
--spec remove_user(User :: ejabberd:user(),
-                  Server :: ejabberd:server(),
-                  Password :: binary()
-                  ) -> ok | not_exists | not_allowed | bad_request | error.
-remove_user(User, Server, _Password) ->
-    case is_user_exists(User, Server) of
-        true -> not_allowed;
-        false -> not_exists
+
+-spec remove_user(User :: ejabberd:luser(),
+                  Server :: ejabberd:lserver()
+                 ) -> ok | {error, not_allowed}.
+remove_user(User, Server) ->
+    case does_user_exist(User, Server) of
+        true -> {error, not_allowed};
+        false -> {error, not_exists}
     end.
 
-%% @doc gen_auth unimplemented callbacks
-login(_User, _Server) -> erlang:error(not_implemented).
-get_password(_User, _Server, _DefaultValue) -> erlang:error(not_implemented).
+
+%% @doc Remove user if the provided password is correct.
+%% This functionality is unused (only called from mod_register) so just stub it out.
+-spec remove_user(User :: ejabberd:luser(),
+                  Server :: ejabberd:lserver(),
+                  Password :: binary()
+                 ) -> ok | {error, not_exists | not_allowed | bad_request}.
+remove_user(User, Server, _Password) ->
+    remove_user(User, Server).
+
+
+-spec plain_password_required() -> boolean().
+plain_password_required() ->
+    false.
+
