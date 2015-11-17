190d8c9b5fcf4c0cf5b9055d1c33b806ab05f863
Phil Toland <phil@hippware.com>
2015-11-16 18:12:49 -0600

    Get eunit, CT, dialyzer and xref working through rebar
    The Makefile is pretty bare at this point. Also, create CT test stubs for
    the wocky code.
    
    Note that the test/ejabberd directory was causing problems with rebar so
    I removed it. The integration test automation still works, it is run from
    the MongooseIM directory rather than being symlinked into the top level
    of the project.

---
+++ apps/wocky/test/cassandra_seestar_SUITE.erl
@@ -0,0 +1,52 @@
+%%% @copyright 2015+ Hippware, Inc.
+%%% @doc Test suite for cassandra_seestar.erl
+-module(cassandra_seestar_SUITE).
+
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+
+
+%%
+%% Suite configuration
+%%
+
+all() -> 
+    [my_test_case].
+
+groups() ->
+    [].
+
+suite() ->
+    [{timetrap, {seconds, 30}}].
+
+
+%%
+%% Setup/Teardown
+%%
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+init_per_group(_GroupName, Config) ->
+    Config.
+
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+
+%%
+%% Testcases
+%%
+
+my_test_case(_Config) -> 
+    ok.
