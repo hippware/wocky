d31e9998e85154e281ed01deae8abab410e924b5
Phil Toland <phil@hippware.com>
2015-11-20 19:37:05 -0600

    Add test suite for wocky_user

--- apps/wocky/test/wocky_user_SUITE.erl
+++ apps/wocky/test/wocky_user_SUITE.erl
@@ -6,16 +6,24 @@
 
 -include_lib("common_test/include/ct.hrl").
 
+-define(DOMAIN, <<"localhost">>).
+-define(USERNAME, <<"bob">>).
+-define(PASSWORD, <<"password">>).
 
 %%
 %% Suite configuration
 %%
 
-all() -> 
-    [my_test_case].
-
-groups() ->
-    [].
+all() -> [
+          create_user_creates_user,
+          create_user_fails_if_user_exists,
+          get_password_gets_password,
+          get_password_fails_if_user_dne,
+          set_password_sets_password,
+          set_password_fails_if_user_dne,
+          remove_user_removes_user,
+          remove_user_succeeds_if_user_dne
+         ].
 
 suite() ->
     [{timetrap, {seconds, 30}}].
@@ -26,21 +34,19 @@ suite() ->
 %%
 
 init_per_suite(Config) ->
+    application:start(wocky),
     Config.
 
 end_per_suite(_Config) ->
-    ok.
-
-init_per_group(_GroupName, Config) ->
-    Config.
-
-end_per_group(_GroupName, _Config) ->
+    application:stop(wocky),
     ok.
 
 init_per_testcase(_TestCase, Config) ->
     Config.
 
 end_per_testcase(_TestCase, _Config) ->
+    {ok, _} = cassandra:pquery(shared, <<"TRUNCATE username_to_user">>, [], quorum),
+    {ok, _} = cassandra:pquery(?DOMAIN, <<"TRUNCATE user">>, [], quorum),
     ok.
 
 
@@ -48,5 +54,47 @@ end_per_testcase(_TestCase, _Config) ->
 %% Testcases
 %%
 
-my_test_case(_Config) -> 
+create_user_creates_user(_Config) ->
+    false = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    ok    = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    true  = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    ok.
+
+create_user_fails_if_user_exists(_Config) ->
+    ok = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    true  = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    {error, exists} = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    ok.
+
+get_password_gets_password(_Config) ->
+    ok = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    ?PASSWORD = wocky_user:get_password(?DOMAIN, ?USERNAME),
+    ok.
+
+get_password_fails_if_user_dne(_Config) ->
+    false = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    {error, not_found} = wocky_user:get_password(?DOMAIN, ?USERNAME),
+    ok.
+
+set_password_sets_password(_Config) ->
+    ok = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    ok = wocky_user:set_password(?DOMAIN, ?USERNAME, <<"newpass">>),
+    <<"newpass">> = wocky_user:get_password(?DOMAIN, ?USERNAME),
+    ok.
+
+set_password_fails_if_user_dne(_Config) ->
+    false = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    {error, not_found} = wocky_user:set_password(?DOMAIN, ?USERNAME, <<"newpass">>),
+    ok.
+
+remove_user_removes_user(_Config) ->
+    ok    = wocky_user:create_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    true  = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    ok    = wocky_user:remove_user(?DOMAIN, ?USERNAME, ?PASSWORD),
+    false = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    ok.
+
+remove_user_succeeds_if_user_dne(_Config) ->
+    false = wocky_user:does_user_exist(?DOMAIN, ?USERNAME),
+    ok    = wocky_user:remove_user(?DOMAIN, ?USERNAME, ?PASSWORD),
     ok.
