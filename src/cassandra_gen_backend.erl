40423c8d5e25b699bcacff4415eb3ebaac71cbd1
Phil Toland <phil@hippware.com>
2015-12-04 15:14:00 -0600

    Flatten the project directory structure

--- src/cassandra_gen_backend.erl
+++ src/cassandra_gen_backend.erl
@@ -8,30 +8,30 @@
 
 -callback clear() -> ok.
 
--callback aquery(Host :: binary(), 
+-callback aquery(Host :: binary(),
                  Query :: binary() | string(),
-                 Values :: [cassandra:value()], 
+                 Values :: [cassandra:value()],
                  Consistency :: cassandra:consistency(),
                  PageSize :: non_neg_integer() | undefined) ->
             {ok, Result :: seestar_result:result()} | {error, Error :: seestar_error:error()}.
 
--callback pquery(Host :: binary(), 
+-callback pquery(Host :: binary(),
                  Query :: binary() | string(),
-                 Values :: [cassandra:value()], 
+                 Values :: [cassandra:value()],
                  Consistency :: cassandra:consistency(),
                  PageSize :: non_neg_integer() | undefined) ->
             {ok, Result :: seestar_result:result()} | {error, Error :: seestar_error:error()}.
 
--callback pquery_async(Host :: binary(), 
+-callback pquery_async(Host :: binary(),
                        Query :: binary() | string(),
-                       Values :: [cassandra:value()], 
+                       Values :: [cassandra:value()],
                        Consistency :: cassandra:consistency(),
                        PageSize :: non_neg_integer() | undefined) ->
             {ok, Result :: seestar_result:result()} | {error, Error :: seestar_error:error()}.
 
--callback batch_pquery(Host :: binary(), 
+-callback batch_pquery(Host :: binary(),
                  Queries :: [{binary() | string(), [cassandra:value()]}],
-                 Type :: logged | unlogged | counter, 
+                 Type :: logged | unlogged | counter,
                  Consistency :: cassandra:consistency()) ->
             {ok, Result :: seestar_result:result()} | {error, Error :: seestar_error:error()}.
 
