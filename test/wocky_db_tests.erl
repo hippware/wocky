18e3707374b12d4dd790d6a00992b62818661ab1
Phil Toland <phil@hippware.com>
2016-01-13 17:04:49 -0600

    Beef up the wocky_db tests a bit

--- test/wocky_db_tests.erl
+++ test/wocky_db_tests.erl
@@ -17,6 +17,13 @@ wocky_db_to_keyspace_test_() -> {
     ]}
 ]}.
 
+wocky_db_invalid_keyspace_test_() -> {
+  "query", [
+    { "should return an error when given an invalid keyspace", [
+      ?_assertMatch({error, _},
+                    wocky_db:query(<<"bogus">>, "SELECT * FROM user", quorum))
+    ]}
+]}.
 
 %% This is a very simple test to verify that basic communication with
 %% Cassandra works. It probably isn't worth testing this functionality more
@@ -32,15 +39,24 @@ wocky_db_api_smoke_test() ->
       [{id, now}, {domain, <<"localhost">>}, {username, <<"bob">>}],
       [{id, now}, {domain, <<"localhost">>}, {username, <<"charlie">>}]
     ],
-    wocky_db:multi_query(shared, Q1, Values, quorum),
+    [{ok, _}, {ok, _}, {ok, _}] =
+        wocky_db:multi_query(shared, Q1, Values, quorum),
+
+    %% You aren't supposed to use batches like this, but this is just a test
+    Queries = [
+      {Q1, [{id, now}, {domain, <<"localhost">>}, {username, <<"dan">>}]},
+      {Q1, [{id, now}, {domain, <<"localhost">>}, {username, <<"ed">>}]},
+      {Q1, [{id, now}, {domain, <<"localhost">>}, {username, <<"frank">>}]}
+    ],
+    {ok, void} = wocky_db:batch_query(shared, Queries, logged, quorum),
 
     Q2 = "SELECT username FROM username_to_user",
     {ok, R1} = wocky_db:query(shared, Q2, quorum),
-    ?assertEqual(3, length(wocky_db:rows(R1))),
+    ?assertEqual(6, length(wocky_db:rows(R1))),
     ?assertEqual(1, length(wocky_db:single_row(R1))),
 
     NotBob = fun (Row) -> proplists:get_value(username, Row) =/= <<"bob">> end,
-    ?assertEqual(2, wocky_db:count(NotBob, R1)),
+    ?assertEqual(5, wocky_db:count(NotBob, R1)),
 
     Q3 = "TRUNCATE username_to_user",
     {ok, _} = wocky_db:query(shared, Q3, quorum),
