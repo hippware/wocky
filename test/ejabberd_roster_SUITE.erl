d420338abc3c8810c14b50c7ef9de40ca371696e
Phil Toland <phil@hippware.com>
2016-01-15 18:15:55 -0600

    Add roster integration test copied from MIM
    Note that only some of these tests pass at this time.

---
+++ test/ejabberd_roster_SUITE.erl
@@ -0,0 +1,272 @@
+%%==============================================================================
+%% Copyright 2013 Erlang Solutions Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%==============================================================================
+
+-module(ejabberd_roster_SUITE).
+-compile(export_all).
+
+-include_lib("escalus/include/escalus.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() -> [
+    {group, roster},
+    {group, subscriptions}
+].
+
+groups() -> [
+    {roster, [sequence], roster_tests()},
+    {subscriptions, [sequence], subscription_tests()}
+].
+
+suite() ->
+    [{required, ejabberd_node} | escalus:suite()].
+
+roster_tests() -> [get_roster,
+                   add_contact,
+                   roster_push].
+
+%%  WARNING: Side-effects & test interference
+%%  subscribe affects subsequent tests
+%%  by sending a directed presence before the roster push
+%%  in add_sample_contact/2
+%%  TODO: investigate, fix.
+
+subscription_tests() -> [unsubscribe,
+                         decline_subscription,
+                         subscribe].
+
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+
+init_per_suite(Config) ->
+    net_kernel:start(['mongooseim@localhost', longnames]),
+
+    wocky_app:start(),
+
+    application:load(ejabberd),
+    DataDir = proplists:get_value(data_dir, Config),
+    ConfigPath = filename:join([DataDir, "ejabberd.cfg"]),
+    application:set_env(ejabberd, config, ConfigPath),
+    application:ensure_all_started(ejabberd),
+
+    escalus:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    escalus:end_per_suite(Config),
+    application:stop(ejabberd),
+    wocky_app:stop(),
+    ok.
+
+init_per_group(_GroupName, Config) ->
+    escalus:create_users(Config, {by_name, [alice, bob]}).
+
+end_per_group(_GroupName, Config) ->
+    escalus:delete_users(Config, {by_name, [alice, bob]}).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(add_contact, Config) ->
+    [{_, UserSpec} | _] = escalus_config:get_config(escalus_users, Config),
+    remove_roster(Config, UserSpec),
+    escalus:end_per_testcase(add_contact, Config);
+end_per_testcase(roster_push, Config) ->
+    [{_, UserSpec} | _] = escalus_config:get_config(escalus_users, Config),
+    remove_roster(Config, UserSpec),
+    escalus:end_per_testcase(roster_push, Config);
+end_per_testcase(subscribe, Config) ->
+    end_rosters_remove(Config);
+end_per_testcase(decline_subscription, Config) ->
+    end_rosters_remove(Config);
+end_per_testcase(unsubscribe, Config) ->
+    end_rosters_remove(Config);
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+end_rosters_remove(Config) ->
+    [{_, UserSpec1}, {_, UserSpec2} | _] =
+        escalus_config:get_config(escalus_users, Config),
+    remove_roster(Config, UserSpec1),
+    remove_roster(Config, UserSpec2),
+    escalus:end_per_testcase(subscription, Config).
+
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+get_roster(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun (Alice, _Bob) ->
+        escalus_client:send(Alice, escalus_stanza:roster_get()),
+        escalus_client:wait_for_stanza(Alice)
+    end).
+
+add_contact(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun (Alice, Bob) ->
+        %% add contact
+        escalus_client:send(Alice,
+                            escalus_stanza:roster_add_contact(Bob,
+                                                              [<<"friends">>],
+                                                              <<"Bobby">>)),
+        Received = escalus_client:wait_for_stanza(Alice),
+        escalus_client:send(Alice, escalus_stanza:iq_result(Received)),
+        escalus_client:wait_for_stanza(Alice)
+    end).
+
+roster_push(Config) ->
+    escalus:story(Config, [{alice, 2}, {bob, 1}], fun (Alice1, Alice2, Bob) ->
+        %% add contact
+        escalus_client:send(Alice1,
+                            escalus_stanza:roster_add_contact(Bob,
+                                                              [<<"friends">>],
+                                                              <<"Bobby">>)),
+        Received = escalus_client:wait_for_stanza(Alice1),
+        escalus_client:send(Alice1, escalus_stanza:iq_result(Received)),
+        escalus_client:wait_for_stanza(Alice1),
+
+        Received2 = escalus_client:wait_for_stanza(Alice2),
+        escalus_client:send(Alice2, escalus_stanza:iq_result(Received2))
+    end).
+
+
+subscribe(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun (Alice, Bob) ->
+        %% add contact
+        add_sample_contact(Alice, Bob),
+
+        %% subscribe
+        escalus_client:send(
+          Alice, escalus_stanza:presence_direct(bob, <<"subscribe">>)),
+        PushReq = escalus_client:wait_for_stanza(Alice),
+        escalus_client:send(Alice, escalus_stanza:iq_result(PushReq)),
+
+        %% Bob receives subscription reqest
+        escalus_client:wait_for_stanza(Bob),
+
+        %% Bob adds new contact to his roster
+        escalus_client:send(Bob,
+                            escalus_stanza:roster_add_contact(Alice,
+                                                              [<<"enemies">>],
+                                                              <<"Alice">>)),
+        PushReqB = escalus_client:wait_for_stanza(Bob),
+        escalus_client:send(Bob, escalus_stanza:iq_result(PushReqB)),
+        escalus_client:wait_for_stanza(Bob),
+
+        %% Bob sends subscribed presence
+        escalus_client:send(
+          Bob, escalus_stanza:presence_direct(alice, <<"subscribed">>)),
+
+        %% Alice receives subscribed
+        escalus_client:wait_for_stanzas(Alice, 3),
+
+        %% Bob receives roster push
+        escalus_client:wait_for_stanza(Bob)
+    end).
+
+decline_subscription(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun (Alice, Bob) ->
+        %% add contact
+        add_sample_contact(Alice, Bob),
+
+        %% subscribe
+        escalus_client:send(
+          Alice, escalus_stanza:presence_direct(bob, <<"subscribe">>)),
+        PushReq = escalus_client:wait_for_stanza(Alice),
+        escalus_client:send(Alice, escalus_stanza:iq_result(PushReq)),
+
+        %% Bob receives subscription reqest
+        escalus_client:wait_for_stanza(Bob),
+
+        %% Bob refuses subscription
+        escalus_client:send(
+          Bob, escalus_stanza:presence_direct(alice, <<"unsubscribed">>)),
+
+        %% Alice receives subscribed
+        escalus_client:wait_for_stanzas(Alice, 2)
+    end).
+
+unsubscribe(Config) ->
+    escalus:story(Config, [{alice, 1}, {bob, 1}], fun (Alice, Bob) ->
+        %% add contact
+        add_sample_contact(Alice, Bob),
+        %% subscribe
+        escalus_client:send(
+          Alice, escalus_stanza:presence_direct(bob, <<"subscribe">>)),
+        PushReq = escalus_client:wait_for_stanza(Alice),
+
+        escalus_client:send(Alice, escalus_stanza:iq_result(PushReq)),
+
+        %% Bob receives subscription reqest
+        escalus_client:wait_for_stanza(Bob),
+        %% Bob adds new contact to his roster
+        escalus_client:send(Bob,
+                            escalus_stanza:roster_add_contact(Alice,
+                                                              [<<"enemies">>],
+                                                              <<"Alice">>)),
+        PushReqB = escalus_client:wait_for_stanza(Bob),
+        escalus_client:send(Bob, escalus_stanza:iq_result(PushReqB)),
+        escalus_client:wait_for_stanza(Bob),
+
+        %% Bob sends subscribed presence
+        escalus_client:send(
+          Bob, escalus_stanza:presence_direct(alice, <<"subscribed">>)),
+
+        %% Alice receives subscribed
+        escalus_client:wait_for_stanzas(Alice, 2),
+
+        escalus_client:wait_for_stanza(Alice),
+
+        %% Bob receives roster push
+        PushReqB1 = escalus_client:wait_for_stanza(Bob),
+        escalus_assert:is_roster_set(PushReqB1),
+
+        %% Alice sends unsubscribe
+        escalus_client:send(
+          Alice, escalus_stanza:presence_direct(bob, <<"unsubscribe">>)),
+
+        PushReqA2 = escalus_client:wait_for_stanza(Alice),
+        escalus_client:send(Alice, escalus_stanza:iq_result(PushReqA2)),
+
+        %% Bob receives unsubscribe
+        escalus_client:wait_for_stanzas(Bob, 2)
+    end).
+
+
+%%-----------------------------------------------------------------
+%% Helpers
+%%-----------------------------------------------------------------
+
+add_sample_contact(Alice, Bob) ->
+    add_sample_contact(Alice, Bob, [<<"friends">>], <<"generic :p name">>).
+
+add_sample_contact(Alice, Bob, Groups, Name) ->
+    escalus_client:send(Alice,
+        escalus_stanza:roster_add_contact(Bob, Groups, Name)),
+    RosterPush = escalus_client:wait_for_stanza(Alice),
+    escalus:assert(is_roster_set, RosterPush),
+    escalus_client:send(Alice, escalus_stanza:iq_result(RosterPush)),
+    escalus_client:wait_for_stanza(Alice).
+
+remove_roster(Config, UserSpec) ->
+    [Username, Server, _Pass] = escalus_users:get_usp(Config, UserSpec),
+    escalus_ejabberd:rpc(mod_wocky_roster, remove_user, [Username, Server]).
