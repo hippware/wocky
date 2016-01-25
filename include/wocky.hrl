801fee1d5ced677a9d4934d8ccaf684a24ae2eba
Phil Toland <phil@hippware.com>
2016-01-25 16:40:33 -0600

    First cut at a high level C* API
    The idea here is to create a high level API for C* that will make existing db code simpler and allow us to do more schema manipulation in code. The latter should make both manual and automated testing easier.
    
    The API as it is now represents about 70% of what we will ultimately need. I am shooting for the simplest thing that will meet the goals and not a full-featured API that covers all of what you can do through CQL. As a result, I am focusing only on generating CQL statements that we use and not covering features that we don't need now.

--- include/wocky.hrl
+++ include/wocky.hrl
@@ -12,4 +12,11 @@
 
 -define(WOCKY_VERSION, element(2, application:get_key(wocky,vsn))).
 
+-record(table_def, {
+          name        :: atom(),
+          columns     :: [{atom(), atom()}],
+          primary_key :: atom() | [atom()],
+          order_by    :: atom() | [atom()]
+         }).
+
 -endif. % ifdef WOCKY_HRL
