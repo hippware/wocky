bbe61b115ab335ddd6d4e9fc4deeb79d3076d9ea
Phil Toland <phil@hippware.com>
2016-02-09 19:03:44 -0600

    Validate that user IDs are valid UUIDs
    If the user IDs are not valid UUIDs and they are used to query the database the cqerl will crash because they are not in the proper format.

--- test/wocky_db_user_tests.erl
+++ test/wocky_db_user_tests.erl
@@ -5,14 +5,16 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("wocky_db_seed.hrl").
 
--import(wocky_db_user, [does_user_exist/2, create_user/3, create_user/4,
-                        remove_user/2, get_password/2, set_password/3]).
+-import(wocky_db_user, [is_valid_id/1, does_user_exist/2, create_user/3,
+                        create_user/4, remove_user/2, get_password/2,
+                        set_password/3]).
 
 
 wocky_db_user_test_() -> {
   "wocky_db_user",
   setup, fun before_all/0, fun after_all/1,
   [
+    test_is_valid_id(),
     test_does_user_exist(),
     test_create_user_without_id(),
     test_create_user_with_id(),
@@ -42,13 +44,28 @@ after_each(_) ->
     ok = wocky_db_seed:clear_tables(?LOCAL_CONTEXT, [user]),
     ok.
 
+test_is_valid_id() ->
+  { "is_valid_id", [
+    { "returns true if the user ID is a valid UUID", [
+      ?_assert(is_valid_id(?USER)),
+      ?_assert(is_valid_id(ossp_uuid:make(v1, text))),
+      ?_assert(is_valid_id(ossp_uuid:make(v1, binary))),
+      ?_assert(is_valid_id(ossp_uuid:make(v4, text))),
+      ?_assert(is_valid_id(ossp_uuid:make(v4, binary)))
+    ]},
+    { "returns false if the user ID is not a valid UUID", [
+      ?_assertNot(is_valid_id(<<"alice">>))
+    ]}
+  ]}.
+
 test_does_user_exist() ->
   { "does_user_exist", setup, fun before_each/0, fun after_each/1, [
     { "returns true if the user exists", [
       ?_assert(does_user_exist(?USER, ?SERVER))
     ]},
     { "returns false if the user does not exist", [
-      ?_assertNot(does_user_exist(?BADUSER, ?SERVER))
+      ?_assertNot(does_user_exist(?BADUSER, ?SERVER)),
+      ?_assertNot(does_user_exist(<<"alice">>, ?SERVER))
     ]}
   ]}.
 
@@ -69,9 +86,13 @@ test_create_user_with_id() ->
                                     <<"nosuchuser">>, ?PASS)),
       ?_assert(does_user_exist(?NEWUSER, ?SERVER))
     ]},
-    { "fails if user already exists", [
+    { "returns {error, exists} if user already exists", [
       ?_assertMatch({error, exists},
                     create_user(?USER, ?SERVER, ?HANDLE, ?PASS))
+    ]},
+    { "returns {error, invalid_id} if user ID is not a valid UUID", [
+      ?_assertMatch({error, invalid_id},
+                    create_user(<<"alice">>, ?SERVER, ?HANDLE, ?PASS))
     ]}
   ]}.
 
@@ -82,6 +103,9 @@ test_get_password() ->
     ]},
     { "returns {error, not_found} if user does not exist", [
       ?_assertMatch({error, not_found}, get_password(?BADUSER, ?SERVER))
+    ]},
+    { "returns {error, not_found} if user ID is not a valid UUID", [
+      ?_assertMatch({error, not_found}, get_password(<<"alice">>, ?SERVER))
     ]}
   ]}.
 
@@ -93,6 +117,10 @@ test_set_password() ->
     ]},
     { "returns {error, not_found} if user does not exist", [
       ?_assertMatch({error, not_found}, set_password(?BADUSER, ?SERVER, ?PASS))
+    ]},
+    { "returns {error, not_found} if user ID is not a valid UUID", [
+      ?_assertMatch({error, not_found},
+                    set_password(<<"alice">>, ?SERVER, ?PASS))
     ]}
   ]}.
 
@@ -104,5 +132,8 @@ test_remove_user() ->
     ]},
     { "succeeds if user does not exist", [
       ?_assertMatch(ok, remove_user(?BADUSER, ?SERVER))
+    ]},
+    { "returns ok if user ID is not a valid UUID", [
+      ?_assertMatch(ok, remove_user(<<"alice">>, ?SERVER))
     ]}
   ]}.
