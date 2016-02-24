651f5ccdf6f41a898e572ebd9a9fc5e51888643b
Bernard Duggan <bernard@hippware.com>
2016-02-24 11:10:39 +1100

    Remove unneeded exports

--- src/mod_mam_wocky.erl
+++ src/mod_mam_wocky.erl
@@ -27,12 +27,6 @@
         ]).
 -endif.
 
-%% Query construction internal exports
-%-export([add_jids/2,
-%         add_times/2,
-%         add_ordering/2,
-%         add_limit/2]).
-
 -type result_row() :: {non_neg_integer(), ejabberd:jid(), exml:element()}.
 -type mam_time() :: {uuid, inclusive | exclusive, binary()} |
                     {time, non_neg_integer()} |
