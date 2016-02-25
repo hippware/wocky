c5f723f059cc1f0608ca48ea7dbcadf400a44403
Phil Toland <phil@hippware.com>
2016-02-24 23:48:52 -0600

    Add IQ handler for auth token management

---
+++ src/mod_wocky_token.erl
@@ -0,0 +1,56 @@
+%%% @copyright 2016+ Hippware, Inc.
+%%% @doc Module to handle token related IQs
+-module(mod_wocky_token).
+
+-include_lib("ejabberd/include/ejabberd.hrl").
+-include_lib("ejabberd/include/jlib.hrl").
+
+%% gen_mod behaviour
+-behaviour(gen_mod).
+-export([start/2, stop/1]).
+
+%% IQ handler callback
+-export([handle_iq/3]).
+
+-define(NS_TOKEN, <<"hippware.com/token">>).
+
+
+%%%===================================================================
+%%% gen_mod implementation
+%%%===================================================================
+
+start(Host, Opts) ->
+    IQDisc = gen_mod:get_opt(iqdisc, Opts, one_queue),
+    gen_iq_handler:add_iq_handler(ejabberd_sm, Host, ?NS_TOKEN,
+                                  ?MODULE, handle_iq, IQDisc).
+
+stop(Host) ->
+    gen_iq_handler:remove_iq_handler(ejabberd_sm, Host, ?NS_TOKEN).
+
+
+%%%===================================================================
+%%% IQ handler callback
+%%%===================================================================
+
+handle_iq(From, _To, #iq{sub_el = SubEl} = IQ) ->
+    #jid{luser = LUser, lserver = LServer, lresource = LResource} = From,
+    case lists:member(LServer, ?MYHOSTS) of
+        true ->
+            handle_local_iq(LUser, LServer, LResource, IQ);
+
+        _ ->
+            IQ#iq{type = error, sub_el = [SubEl, ?ERR_ITEM_NOT_FOUND]}
+    end.
+
+handle_local_iq(LUser, LServer, LResource, #iq{type = get} = IQ) ->
+    {ok, Token} = wocky_db_user:assign_token(LUser, LServer, LResource),
+    iq_result(IQ, [#xmlcdata{content = Token}]);
+handle_local_iq(LUser, LServer, LResource, #iq{type = set} = IQ) ->
+    ok = wocky_db_user:release_token(LUser, LServer, LResource),
+    iq_result(IQ, []).
+
+iq_result(IQ, Content) ->
+    IQ#iq{type = result,
+          sub_el = [#xmlel{name = <<"query">>,
+                           attrs = [{<<"xmlns">>, ?NS_TOKEN}],
+                           children = Content}]}.
