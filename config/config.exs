2645b5e2d0859022523a98950d1e04b8f23c40bc
Phil Toland <phil@hippware.com>
2016-07-07 18:34:22 -0500

    Add Wocky configs using Mix.Config

---
+++ config/config.exs
@@ -0,0 +1,52 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+config :wocky,
+  start_ejabberd: :true,
+  keyspace_prefix: 'wocky_'
+
+config :cqerl,
+  maps: :true,
+  mode: :hash,
+  text_uuids: true,
+  auth: {:cqerl_auth_plain_handler, [{'cassandra', 'cassandra'}]}
+
+config :kernel,
+  start_pg2: :true
+
+config :sasl,
+  sasl_error_logger: :false
+
+config :mnesia,
+  dir: 'data/mnesia'
+
+config :setup,
+  verify_directories: :false
+
+config :lager,
+  colored: :true,
+  log_root: 'log',
+  crash_log: 'crash.log',
+  handlers: [
+    {:lager_console_backend, [
+       :info,
+       {:lager_default_formatter, [:time, :color, ' [', :severity, '] ', :message, '\e[0m\r\n']}
+    ]},
+    {:lager_file_backend, [
+      {:file, 'wocky.log'},
+      {:level, :warning},
+      {:size, 2097152},
+      {:date, '$D0'},
+      {:count, 5}
+    ]},
+    {:lager_file_backend, [
+      {:file, 'debug.log'},
+      {:level, :debug},
+      {:size, 2097152},
+      {:date, '$D0'},
+      {:count, 5}
+    ]}
+  ]
+
+import_config "#{Mix.env}.exs"
