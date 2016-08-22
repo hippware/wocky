0d9e851a1b7bb36310544c1f2eeaec5d2692966a
Phil Toland <phil@hippware.com>
2016-08-22 15:20:48 -0500

    Use proper Keyword lists for Lager configuration

--- config/config.exs
+++ config/config.exs
@@ -33,24 +33,24 @@ config :lager,
   log_root: '_build/#{Mix.env}/log',
   crash_log: 'crash.log',
   handlers: [
-    {:lager_console_backend, [
+    lager_console_backend: [
        :info,
        {:lager_default_formatter, [:time, :color, ' [', :severity, '] ', :message, '\e[0m\r\n']}
-    ]},
-    {:lager_file_backend, [
-      {:file, 'wocky.log'},
-      {:level, :warning},
-      {:size, 2097152},
-      {:date, '$D0'},
-      {:count, 5}
-    ]},
-    {:lager_file_backend, [
-      {:file, 'debug.log'},
-      {:level, :debug},
-      {:size, 2097152},
-      {:date, '$D0'},
-      {:count, 5}
-    ]}
+    ],
+    lager_file_backend: [
+      file: 'wocky.log',
+      level: :warning,
+      size: 2097152,
+      date: '$D0',
+      count: 5
+    ],
+    lager_file_backend: [
+      file: 'debug.log',
+      level: :debug,
+      size: 2097152,
+      date: '$D0',
+      count: 5
+    ]
   ]
 
 config :hut,
