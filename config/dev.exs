43f64f21030d4a4ec9d11c0dd53353db7690f0d0
Phil Toland <phil@hippware.com>
2016-08-22 15:23:22 -0500

    Redirect error_logger output in the dev environment
    This cuts way back on the log noise when starting up Wocky.

--- config/dev.exs
+++ config/dev.exs
@@ -1,2 +1,11 @@
 use Mix.Config
 
+
+config :lager,
+  extra_sinks: [
+    error_logger_lager_event: [
+      handlers: [
+        lager_file_backend: [file: 'error_logger.log', level: :info]
+      ]
+    ]
+  ]
