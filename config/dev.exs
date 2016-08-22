ae6f7b2fe87dae0e85796d54b7fd557288d1b6c7
Phil Toland <phil@hippware.com>
2016-08-22 15:27:16 -0500

    Configure the db for dev in the mix config file
    but create a "local.cfg" file for talking to a local db when running a release.

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,25 @@
 use Mix.Config
 
+config :schemata,
+  cluster: [
+    username: 'cassandra',
+    password: 'cassandra',
+    seed_hosts: [
+      {'127.0.0.1', 9042}
+    ],
+    keyspaces: [
+      wocky_shared: [
+        strategy: :simple,
+        factor: 1,
+        clients: 10
+      ],
+      wocky_localhost: [
+        strategy: :simple,
+        factor: 1,
+        clients: 10
+      ]
+    ]
+  ]
 
 config :lager,
   extra_sinks: [
