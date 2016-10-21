e6d39349c9b6c5f3f6a9a0484c618458c7e794a4
Bernard Duggan <bernard@hippware.com>
2016-10-21 19:22:13 +0900

    Add multicast support (XEP-0033 subset)

---
+++ src/mod_wocky_multicast.erl
@@ -0,0 +1,113 @@
+%%% @copyright 2016+ Hippware, Inc.
+%%%
+%%% @doc Module implementing Wocky bots
+%%% See https://github.com/hippware/tr-wiki/wiki/Bot
+%%%
+-module(mod_wocky_multicast).
+
+-behaviour(gen_mod).
+
+-compile({parse_transform, do}).
+-compile({parse_transform, cut}).
+
+%% gen_mod handlers
+-export([start/2, stop/1]).
+
+-include_lib("ejabberd/include/jlib.hrl").
+-include_lib("ejabberd/include/ejabberd.hrl").
+-include("wocky_roster.hrl").
+
+%%%===================================================================
+%%% gen_mod handlers
+%%%===================================================================
+
+start(Host, _Opts) ->
+    mod_disco:register_feature(Host, ?NS_ADDRESS),
+    ejabberd_hooks:add(filter_local_packet, Host,
+                       fun filter_local_packet_hook/1, 80).
+
+
+stop(Host) ->
+    mod_disco:unregister_feature(Host, ?NS_ADDRESS),
+    ejabberd_hooks:delete(filter_local_packet, Host,
+                          fun filter_local_packet_hook/1, 80).
+
+%%%===================================================================
+%%% Incoming packet handler
+%%%===================================================================
+
+-type filter_packet() :: {ejabberd:jid(), ejabberd:jid(), jlib:xmlel()}.
+-spec filter_local_packet_hook(filter_packet() | drop) ->
+    filter_packet() | drop.
+filter_local_packet_hook(P = {_, _, Packet = #xmlel{name = Name}})
+      when Name =:= <<"message">> orelse
+           Name =:= <<"presence">> ->
+    case xml:get_subtag(Packet, <<"addresses">>) of
+        false -> P;
+        AddressesEl -> multicast_packet(P, AddressesEl)
+    end;
+filter_local_packet_hook(Other) ->
+    Other.
+
+multicast_packet(P = {From, To, Packet}, AddressesEl) ->
+    case multicast_packet(From, To, Packet, AddressesEl) of
+        ok -> drop;
+        {error, _} -> P
+    end.
+
+multicast_packet(From, To, Packet, AddressesEl) ->
+    do([error_m ||
+        check_ns(AddressesEl),
+        check_to(From, To),
+        Addresses <- get_addresses(From, AddressesEl),
+        CleanPacket <- strip_addresses(Packet, AddressesEl),
+        forward_message(From, Addresses, CleanPacket)
+       ]).
+
+check_ns(AddressesEl) ->
+    case xml:get_tag_attr(<<"xmlns">>, AddressesEl) of
+        {value, ?NS_ADDRESS} -> ok;
+        _ -> {error, not_address_ns}
+    end.
+
+check_to(#jid{lserver = LServer},
+         #jid{luser = <<>>, lserver = LServer, lresource = <<>>}) ->
+    case wocky_app:server() of
+        LServer -> ok;
+        _ -> {error, not_local_server}
+    end;
+check_to(_, _) ->
+    {error, not_users_server}.
+
+get_addresses(From, AddressesEl) ->
+    {ok, lists:usort(
+           lists:foldl(
+             get_address(From, _, _), [], AddressesEl#xmlel.children))}.
+
+get_address(From, #xmlel{name = <<"address">>, attrs = Attrs}, Acc) ->
+    case xml:get_attr(<<"type">>, Attrs) of
+        {value, <<"friends">>} ->
+            get_contact_addresses(
+              wocky_db_roster:is_friend(_), From) ++ Acc;
+        {value, <<"followers">>} ->
+            get_contact_addresses(
+              wocky_db_roster:is_follower(_), From) ++ Acc;
+        {value, Type} ->
+            ok = lager:warning("Unsupported address type received: ~p",
+                               [Type]),
+            Acc;
+        false ->
+            ok = lager:warning("Missing type attribute on address element"),
+            Acc
+    end.
+
+get_contact_addresses(FilterFun, #jid{luser = LUser, lserver = LServer}) ->
+    Roster = wocky_db_roster:get_roster(LUser, LServer),
+    Filtered = lists:filter(FilterFun, Roster),
+    [jid:make(CJ) || #wocky_roster{contact_jid = CJ} <- Filtered].
+
+strip_addresses(Packet = #xmlel{children = Children}, AddressesEl) ->
+    {ok, Packet#xmlel{children = lists:delete(AddressesEl, Children)}}.
+
+forward_message(From, Tos, Packet) ->
+    lists:foreach(ejabberd_router:route(From, _, Packet), Tos).
---
+++ test/multicast_SUITE.erl
@@ -0,0 +1,135 @@
+%%% @copyright 2016+ Hippware, Inc.
+%%% @doc Integration test suite for multicast addressing
+-module(multicast_SUITE).
+-compile(export_all).
+-compile({parse_transform, fun_chain}).
+
+-include_lib("ejabberd/include/jlib.hrl").
+-include_lib("common_test/include/ct.hrl").
+-include_lib("stdlib/include/assert.hrl").
+
+-include("wocky_db_seed.hrl").
+
+-compile({parse_transform, cut}).
+
+%%--------------------------------------------------------------------
+%% Suite configuration
+%%--------------------------------------------------------------------
+
+all() -> [friends,
+          followers,
+          error
+         ].
+
+suite() ->
+    escalus:suite().
+
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+users() ->
+    [alice, bob, carol, karen, robert, tim].
+
+init_per_suite(Config) ->
+    ok = test_helper:ensure_wocky_is_running(),
+    wocky_db:clear_user_tables(?LOCAL_CONTEXT),
+    Users = escalus:get_users(users()),
+    fun_chain:first(Config,
+        escalus:init_per_suite(),
+        escalus:create_users(Users)
+    ).
+
+end_per_suite(Config) ->
+    escalus:delete_users(Config, escalus:get_users(users())),
+    escalus:end_per_suite(Config).
+
+init_per_testcase(CaseName, Config) ->
+    escalus:init_per_testcase(CaseName, Config).
+
+end_per_testcase(CaseName, Config) ->
+    escalus:end_per_testcase(CaseName, Config).
+
+friends(Config) ->
+    %% Alice sends a message to Bob, who is offline
+    escalus:story(Config, [{alice, 1}, {bob, 1}, {carol, 1},
+                           {robert, 1}, {karen, 1}, {tim, 1}],
+      fun(Alice, Bob, Carol, Robert, Karen, Tim) ->
+        %% Make Alice friends with Bob and Carol
+        lists:foreach(
+          test_helper:subscribe_pair(_, Alice),
+          [Bob, Carol]),
+
+        %% Make Tim a follower of Alice
+        test_helper:subscribe(Tim, Alice),
+
+        SendStanza = multicast_stanza(<<"friends">>),
+
+        escalus:send(Alice, SendStanza),
+
+        expect_multicast_msg([Bob, Carol]),
+
+        %% Tim is not a friend, so gets nothing
+        test_helper:ensure_all_clean([Robert, Karen, Tim])
+    end).
+
+followers(Config) ->
+    ok = wocky_db:clear_tables(shared, [roster]),
+    %% Alice sends a message to Bob, who is offline
+    escalus:story(Config, [{alice, 1}, {bob, 1}, {tim, 1}, {robert, 1}],
+      fun(Alice, Bob, Tim, Robert) ->
+        test_helper:subscribe_pair(Bob, Alice),
+        test_helper:subscribe(Tim, Alice),
+        SendStanza = multicast_stanza(<<"followers">>),
+
+        escalus:send(Alice, SendStanza),
+
+        expect_multicast_msg([Bob, Tim]),
+        test_helper:ensure_all_clean([Robert])
+      end).
+
+error(Config) ->
+    ok = wocky_db:clear_tables(shared, [roster]),
+    escalus:story(Config, [{alice, 1}, {bob, 1}, {tim, 1}],
+      fun(Alice, Bob, Tim) ->
+        test_helper:subscribe_pair(Bob, Alice),
+        test_helper:subscribe(Tim, Alice),
+
+        %% Unknown multicast address
+        escalus:send(Alice, multicast_stanza(<<"fnord">>)),
+
+        %% Wrong NS
+        escalus:send(Alice, multicast_stanza([{<<"type">>, <<"friends">>}],
+                                             <<"wrongNS">>)),
+
+        %% Missing type
+        escalus:send(Alice, multicast_stanza([], ?NS_ADDRESS)),
+
+        test_helper:ensure_all_clean([Alice, Bob, Tim])
+      end).
+
+
+multicast_stanza(Type) ->
+    multicast_stanza([{<<"type">>, Type}], ?NS_ADDRESS).
+
+multicast_stanza(AddressAttrs, NS) ->
+    Stanza = escalus_stanza:chat_to(
+               ?LOCAL_CONTEXT, <<"Check out my LOLCAT">>),
+    Address = #xmlel{name = <<"address">>,
+                     attrs = AddressAttrs},
+    Addresses = #xmlel{name = <<"addresses">>,
+                       attrs = [{<<"xmlns">>, NS}],
+                       children = [Address]},
+    Stanza#xmlel{children =
+                 [Addresses | Stanza#xmlel.children]}.
+
+
+
+expect_multicast_msg(Recipients) ->
+    lists:foreach(
+      fun(C) ->
+        RecStanza = escalus_client:wait_for_stanza(C),
+        ?assertEqual(false,
+                     xml:get_subtag(RecStanza, <<"addresses">>))
+      end, Recipients).
