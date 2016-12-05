d1ac229f5eb8f30404f8cf1611c6f24c2bbc6eaa
Phil Toland <phil@hippware.com>
2016-12-05 13:59:36 -0600

    Re-enable running specs from exguard

--- .exguard.exs
+++ .exguard.exs
@@ -1,12 +1,12 @@
 use ExGuard.Config
 
-# "specs"
-# |> guard(run_on_start: true)
-# |> command("mix espec")
-# |> watch({~r{lib/(?<dir>.+)/(?<file>.+).ex$},
-#     fn (m) -> "spec/#{m["dir"]}/#{m["file"]}_spec.exs" end})
-# |> watch(~r{\.(erl|ex|exs|eex|xrl|yrl)\z}i)
-# |> ignore(~r|db/migrations/|)
+"specs"
+|> guard(run_on_start: true)
+|> command("mix espec")
+|> watch({~r{lib/(?<dir>.+)/(?<file>.+).ex$},
+    fn (m) -> "spec/#{m["dir"]}/#{m["file"]}_spec.exs" end})
+|> watch(~r{\.(erl|ex|exs|eex|xrl|yrl)\z}i)
+|> ignore(~r|db/migrations/|)
 
 "compile"
 |> guard(run_on_start: true)
