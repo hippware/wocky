84aeada1bc9e2cb3c85415d0ae7c79c055130201
Bernard Duggan <bernard@hippware.com>
2017-01-25 08:51:27 +1100

    Remove obsolete tests

--- test/location_SUITE.erl
+++ test/location_SUITE.erl
@@ -11,11 +11,6 @@
 -include("test_helper.hrl").
 -include("wocky_bot.hrl").
 
--import(test_helper, [expect_iq_success_u/3,
-                      get_hs_stanza/0,
-                      check_hs_result/2,
-                      check_hs_result/4]).
-
 
 %%--------------------------------------------------------------------
 %% Suite configuration
@@ -23,9 +18,7 @@
 
 all() -> [geoloc,
           bad_geoloc,
-          end_geoloc,
-          xmpp_notification,
-          bot_follower_notification
+          end_geoloc
          ].
 
 suite() ->
@@ -97,67 +90,6 @@ end_geoloc(Config) ->
         Received = escalus:wait_for_stanzas(Alice, 2),
         escalus:assert_many([is_message, is_iq_result], Received),
         test_helper:ensure_all_clean([Bob, Carol])
-    end).
-
-xmpp_notification(Config) ->
-    escalus:story(Config, [{alice, 1}], fun(Alice) ->
-        wocky_db:truncate(?LOCAL_CONTEXT, home_stream),
-        insert_bot(Alice),
-        enable_push_notifications(Alice),
-
-        %% Alice's home stream is empty to start
-        Stanza = expect_iq_success_u(get_hs_stanza(), Alice, Alice),
-        check_hs_result(Stanza, 0, 0, false),
-
-        %% Alice sends an XMPP location update...
-        Stanza2 = escalus_pubsub_stanza:publish(Alice, <<"abcedfg">>,
-                                                geoloc_item(loc_test),
-                                                <<"123">>,
-                                                {pep, ?NS_GEOLOC}),
-        escalus:send(Alice, Stanza2),
-        Received = escalus:wait_for_stanzas(Alice, 2),
-        escalus:assert_many([is_message, is_iq_result], Received),
-
-        %% ...and one new item shows up in the home stream. This is the
-        %% location update. There is no bot entry notification because
-        %% Alice is both the owner of the bot and the user entering it.
-        Stanza3 = expect_iq_success_u(get_hs_stanza(), Alice, Alice),
-        Items = check_hs_result(Stanza3, 1),
-
-        %% ...with the last item being a message.
-        escalus:assert(is_message, hd((lists:last(Items))#item.stanzas))
-    end).
-
-bot_follower_notification(Config) ->
-    wocky_db_seed:seed_tables(shared, [bot, bot_subscriber]),
-    escalus:story(Config, [{alice, 1}, {carol, 1}], fun(Alice, Carol) ->
-        enable_push_notifications(Alice),
-        test_helper:subscribe_pair(Alice, Carol),
-
-        bot_SUITE:set_visibility(Alice, ?WOCKY_BOT_VIS_OPEN, ?BOT),
-
-        wocky_db:truncate(?LOCAL_CONTEXT, home_stream),
-        wocky_db:truncate(?LOCAL_CONTEXT, bot_event),
-
-        %% Carol sends an XMPP location update...
-        Stanza2 = escalus_pubsub_stanza:publish(Carol, <<"abcedfg">>,
-                                                geoloc_item(bot_test),
-                                                <<"123">>,
-                                                {pep, ?NS_GEOLOC}),
-        escalus:send(Carol, Stanza2),
-        Received = escalus:wait_for_stanzas(Carol, 2),
-        escalus:assert_many([is_message, is_iq_result], Received),
-        timer:sleep(500),
-
-        %% ...and one new items show up in each user's home stream:
-        %% Alice gets the bot entry notification; Carol gets the location
-        %% notification.
-        lists:foreach(
-          fun (Client) ->
-              Stanza = expect_iq_success_u(get_hs_stanza(), Client, Client),
-              Items = check_hs_result(Stanza, 1),
-              escalus:assert(is_message, hd((lists:last(Items))#item.stanzas))
-          end, [Alice, Carol])
     end).
 
 %%--------------------------------------------------------------------
