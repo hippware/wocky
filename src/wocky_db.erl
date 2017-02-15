de82befa2596ce5478dd25fef6689b3860e21b0d
Phil Toland <phil@hippware.com>
2017-02-15 15:38:41 -0600

    Remove last activity and offline message tests
    We can rely on MIM to ship working offline message and last activity modules.

--- src/wocky_db.erl
+++ src/wocky_db.erl
@@ -145,8 +145,8 @@ clear_tables(Context, Tables) ->
 -spec clear_user_tables(context()) -> ok.
 clear_user_tables(Context) ->
     clear_tables(shared, [user, handle_to_user, phone_number_to_user, roster]),
-    clear_tables(Context, [auth_token, last_activity, offline_msg,
-                           privacy, privacy_item, location, conversation]).
+    clear_tables(Context, [auth_token, privacy, privacy_item, location,
+                           conversation]).
 
 
 %% @doc Retrieves a single value from a table based on the parameters.
