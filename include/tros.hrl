2ee7e0c76c3cabf64b2e2779cf983f607609bb68
Bernard Duggan <bernard@hippware.com>
2017-02-27 10:58:04 +1100

    Update delete function to remove all versions of a file

---
+++ include/tros.hrl
@@ -0,0 +1,8 @@
+%%% @copyright 2016+ Hippware, Inc.
+-ifndef(TROS_HRL).
+-define(TROS_HRL, 1).
+
+-define(TROS_THUMBNAIL_SUFFIX, <<"-thumbnail">>).
+-define(TROS_ORIGINAL_SUFFIX, <<"-original">>).
+
+-endif. % ifdef TROS_HRL
