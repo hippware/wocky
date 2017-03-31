5fa8f7dd1c8af4d5130ecc654d17d020442fe780
Phil Toland <phil@hippware.com>
2017-03-31 13:08:28 -0500

    Fix a badarith error during bot search
    The lat and lon are being passed into the bot search as strings, so we need to convert them to floats.
    
    The fix I provided is a bit Q+D. If the string representation is actually an integer, it will throw an error. We shouldn't be getting whole degrees, so it shouldn't be an issue; but it is something to be aware of.

--- apps/emet/lib/wocky/geoutils.ex
+++ apps/emet/lib/wocky/geoutils.ex
@@ -1,10 +1,14 @@
 defmodule Wocky.GeoUtils do
   @moduledoc "Geographic utilities for Wocky"
 
-  @type degrees :: integer | float
+  @type degrees :: binary | float
 
   @doc "Normalise degrees to the range [-180,180]"
   @spec normalize_degrees(degrees) :: degrees
+  def normalize_degrees(degrees) when is_binary(degrees) do
+    # Note that this will throw an error if degrees is an integer
+    degrees |> String.to_float |> normalize_degrees()
+  end
   def normalize_degrees(degrees) when degrees < -180 do
     normalize_degrees(degrees + 2 * 180)
   end
