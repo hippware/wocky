ead5590a289b3a6b98172bed4b36f801dbaf8ffe
Bernard Duggan <bernard@hippware.com>
2017-05-02 13:50:28 +1000

    Add back original MIM functionality

--- apps/wocky/priv/repo/migrations/20170327134450_mim_schema.exs
+++ apps/wocky/priv/repo/migrations/20170327134450_mim_schema.exs
@@ -16,139 +16,166 @@ defmodule Wocky.Repo.Migrations.MIMSchema do
     ) ;\
     """
 
-        execute """
-        CREATE TABLE last (\
-        username varchar(250) PRIMARY KEY, \
-        seconds int NOT NULL, \
-        state text NOT NULL\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE rosterusers (\
-        username varchar(250) NOT NULL, \
-        jid varchar(250) NOT NULL, \
-        nick text NOT NULL, \
-        subscription character(1) NOT NULL, \
-        ask character(1) NOT NULL, \
-        askmessage text NOT NULL, \
-        server character(1) NOT NULL, \
-        subscribe text NOT NULL, \
-        type text, \
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE rostergroups (\
-        username varchar(250) NOT NULL, \
-        jid varchar(250) NOT NULL, \
-        grp text NOT NULL\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE roster_version (\
-        username varchar(250) PRIMARY KEY, \
-        version text NOT NULL\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE offline_message(\
-        id BIGINT NOT NULL PRIMARY KEY, \
-        timestamp BIGINT NOT NULL, \
-        expire BIGINT , \
-        server varchar(250) NOT NULL, \
-        username varchar(250) NOT NULL, \
-        from_jid varchar(250) NOT NULL, \
-        packet bytea NOT NULL\
-        );\
-        """
-    
-        execute """
-        CREATE TABLE privacy_default_list (\
-        username varchar(250) PRIMARY KEY, \
-        name varchar(250) NOT NULL\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE privacy_list (\
-        username varchar(250) NOT NULL, \
-        name varchar(250) NOT NULL, \
-        id BIGINT NOT NULL UNIQUE, \
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
-        PRIMARY KEY (username, name)\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE privacy_list_data (\
-        id bigint, \
-        t character(1) NOT NULL, \
-        value text NOT NULL, \
-        action character(1) NOT NULL, \
-        ord bigint NOT NULL, \
-        match_all boolean NOT NULL, \
-        match_iq boolean NOT NULL, \
-        match_message boolean NOT NULL, \
-        match_presence_in boolean NOT NULL, \
-        match_presence_out boolean NOT NULL, \
-        PRIMARY KEY (id, ord)\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE private_storage (\
-        username varchar(250) NOT NULL, \
-        namespace varchar(250) NOT NULL, \
-        data text NOT NULL, \
-        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP\
-        ) ;\
-        """
-    
-        execute """
-        CREATE TABLE mam_message(\
-        id BIGINT NOT NULL, \
-        user_id INT NOT NULL, \
-        from_jid varchar(250) NOT NULL, \
-        remote_bare_jid varchar(250) NOT NULL, \
-        remote_resource varchar(250) NOT NULL, \
-        direction character(1) NOT NULL, \
-        message bytea NOT NULL, \
-        PRIMARY KEY (user_id, id) \
-        );\
-        """
-    
-        execute """
-        CREATE TABLE mam_config(\
-        user_id INT NOT NULL, \
-        remote_jid varchar(250) NOT NULL, \
-        behaviour character(1) NOT NULL, \
-        PRIMARY KEY (user_id, remote_jid)\
-        );\
-        """
-    
-        execute """
-        CREATE TABLE mam_server_user(\
-        id INT NOT NULL , \
-        server varchar(250) NOT NULL, \
-        user_name varchar(250) NOT NULL, \
-        PRIMARY KEY(id) , \
-        CONSTRAINT uc_mam_server_user_name UNIQUE (server, user_name)\
-        );\
-        """
-    
-        execute """
-        CREATE TABLE mam_muc_message(\
-        id BIGINT NOT NULL, \
-        room_id INT NOT NULL, \
-        nick_name varchar(250) NOT NULL, \
-        message bytea NOT NULL, \
-        PRIMARY KEY (room_id, id)\
-        );\
-        """
+    execute """
+    CREATE TABLE last (\
+    username varchar(250) PRIMARY KEY, \
+    seconds int NOT NULL, \
+    state text NOT NULL\
+    ) ;\
+    """
+    execute "CREATE INDEX i_last_seconds ON last(seconds);"
+
+    execute """
+    CREATE TABLE rosterusers (\
+    username varchar(250) NOT NULL, \
+    jid varchar(250) NOT NULL, \
+    nick text NOT NULL, \
+    subscription character(1) NOT NULL, \
+    ask character(1) NOT NULL, \
+    askmessage text NOT NULL, \
+    server character(1) NOT NULL, \
+    subscribe text NOT NULL, \
+    type text, \
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP\
+    ) ;\
+    """
+    execute """
+    CREATE UNIQUE INDEX i_rosteru_user_jid ON\
+     rosterusers(username, jid);\
+    """
+    execute "CREATE INDEX i_rosteru_username ON rosterusers(username);"
+    execute "CREATE INDEX i_rosteru_jid ON rosterusers(jid);"
+
+    execute """
+    CREATE TABLE rostergroups (\
+    username varchar(250) NOT NULL, \
+    jid varchar(250) NOT NULL, \
+    grp text NOT NULL\
+    ) ;\
+    """
+    execute """
+    CREATE INDEX pk_rosterg_user_jid ON rostergroups(username, jid);\
+    """
+
+    execute """
+    CREATE TABLE roster_version (\
+    username varchar(250) PRIMARY KEY, \
+    version text NOT NULL\
+    ) ;\
+    """
+
+    execute "CREATE SEQUENCE offline_id_seq;"
+    execute """
+    CREATE TABLE offline_message(\
+    id BIGINT NOT NULL DEFAULT nextval('offline_id_seq') PRIMARY KEY, \
+    timestamp BIGINT NOT NULL, \
+    expire BIGINT , \
+    server varchar(250) NOT NULL, \
+    username varchar(250) NOT NULL, \
+    from_jid varchar(250) NOT NULL, \
+    packet bytea NOT NULL\
+    );\
+    """
+    execute """
+    CREATE INDEX i_offline_message ON\
+     offline_message(server, username, id);\
+    """
+
+    execute """
+    CREATE TABLE privacy_default_list (\
+    username varchar(250) PRIMARY KEY, \
+    name varchar(250) NOT NULL\
+    ) ;\
+    """
+
+    execute "CREATE SEQUENCE privacy_list_id_seq;"
+    execute """
+    CREATE TABLE privacy_list (\
+    username varchar(250) NOT NULL, \
+    name varchar(250) NOT NULL, \
+    id BIGINT NOT NULL DEFAULT nextval('privacy_list_id_seq') UNIQUE, \
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+    PRIMARY KEY (username, name)\
+    ) ;\
+    """
+
+    execute """
+    CREATE TABLE privacy_list_data (\
+    id bigint, \
+    t character(1) NOT NULL, \
+    value text NOT NULL, \
+    action character(1) NOT NULL, \
+    ord bigint NOT NULL, \
+    match_all boolean NOT NULL, \
+    match_iq boolean NOT NULL, \
+    match_message boolean NOT NULL, \
+    match_presence_in boolean NOT NULL, \
+    match_presence_out boolean NOT NULL, \
+    PRIMARY KEY (id, ord)\
+    ) ;\
+    """
+
+    execute """
+    CREATE TABLE private_storage (\
+    username varchar(250) NOT NULL, \
+    namespace varchar(250) NOT NULL, \
+    data text NOT NULL, \
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP\
+    ) ;\
+    """
+    execute """
+    CREATE INDEX i_private_storage_username ON\
+     private_storage(username);\
+    """
+    execute """
+    CREATE UNIQUE INDEX i_private_storage_username_namespace ON\
+     private_storage(username, namespace);\
+    """
+
+    execute "CREATE TYPE direction AS ENUM ('I', 'O');"
+    execute """
+    CREATE TABLE mam_message(\
+    id BIGINT NOT NULL, \
+    user_id INT NOT NULL, \
+    from_jid varchar(250) NOT NULL, \
+    remote_bare_jid varchar(250) NOT NULL, \
+    remote_resource varchar(250) NOT NULL, \
+    direction direction NOT NULL, \
+    message bytea NOT NULL, \
+    PRIMARY KEY (user_id, id) \
+    );\
+    """
+
+    execute "CREATE TYPE behaviour AS ENUM ('A', 'N', 'R');"
+    execute """
+    CREATE TABLE mam_config(\
+    user_id INT NOT NULL, \
+    remote_jid varchar(250) NOT NULL, \
+    behaviour behaviour NOT NULL, \
+    PRIMARY KEY (user_id, remote_jid)\
+    );\
+    """
+
+    execute "CREATE SEQUENCE mam_server_user_id_seq;"
+    execute """
+    CREATE TABLE mam_server_user(\
+    id INT NOT NULL DEFAULT nextval('mam_server_user_id_seq'), \
+    server varchar(250) NOT NULL, \
+    user_name varchar(250) NOT NULL, \
+    PRIMARY KEY(id) , \
+    CONSTRAINT uc_mam_server_user_name UNIQUE (server, user_name)\
+    );\
+    """
+
+    execute """
+    CREATE TABLE mam_muc_message(\
+    id BIGINT NOT NULL, \
+    room_id INT NOT NULL, \
+    nick_name varchar(250) NOT NULL, \
+    message bytea NOT NULL, \
+    PRIMARY KEY (room_id, id)\
+    );\
+    """
   end
 
   def down do
