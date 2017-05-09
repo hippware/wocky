0bf946aedf71d73d830a5027fb51b3de8aabcb76
Phil Toland <phil@hippware.com>
2017-05-09 17:42:27 -0500

    Add specs for the timestamp module

--- apps/wocky/spec/wocky/repo/timestamp_spec.exs
+++ apps/wocky/spec/wocky/repo/timestamp_spec.exs
@@ -1,12 +1,29 @@
 defmodule Wocky.Repo.TimestampSpec do
   use ESpec, async: true
 
+  alias Wocky.Repo.Timestamp
+
+  @iso8601_rx ~r/\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\+\d\d:\d\d/
+
   describe "now/1" do
+    subject do: Timestamp.now
+
+    it do: should(be_integer())
   end
 
-  describe "exipired?/1" do
+  describe "expired?/1" do
+    it do: assert Timestamp.expired?(Timestamp.now - 100)
+    it do: refute Timestamp.expired?(Timestamp.now + 100)
   end
 
   describe "to_string/1" do
+    it do: Timestamp.now |> Timestamp.to_string |> should(match @iso8601_rx)
+
+    it do
+      Timestamp.now
+      |> Timex.from_unix
+      |> Timestamp.to_string
+      |> should(match @iso8601_rx)
+    end
   end
 end
