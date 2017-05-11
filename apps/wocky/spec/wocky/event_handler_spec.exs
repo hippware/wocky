2d6a703ca3f86a58ff2c356872172db59d121f75
Phil Toland <phil@hippware.com>
2017-05-10 19:44:44 -0500

    Add test for EventHandler

--- apps/wocky/spec/wocky/event_handler_spec.exs
+++ apps/wocky/spec/wocky/event_handler_spec.exs
@@ -1,7 +1,35 @@
 defmodule Wocky.EventHandlerSpec do
   use ESpec, async: true
 
-  describe "broadcast/1" do
+  defmodule TestEvent do
+    defstruct [:data]
+  end
 
+  defmodule TestEventHandler do
+    use GenStage
+
+    def start_link(pid), do: GenStage.start_link(__MODULE__, pid)
+
+    def init(pid), do: {:consumer, pid, subscribe_to: [EventHandler]}
+
+    def handle_events(events, _from, pid) do
+      _ =
+        events
+        |> Task.async_stream(fn(e) -> handle_event(e, pid) end)
+        |> Enum.to_list
+
+      {:noreply, [], pid}
+    end
+
+    defp handle_event(%TestEvent{data: data}, pid), do: send(pid, data)
+    defp handle_event(_, _), do: :ok
+  end
+
+  describe "broadcast/1" do
+    it "should dispatch to event handlers" do
+      {:ok, _pid} = TestEventHandler.start_link(self())
+      Wocky.EventHandler.broadcast(%TestEvent{data: :test})
+      assert_receive(:test, 200)
+    end
   end
 end
