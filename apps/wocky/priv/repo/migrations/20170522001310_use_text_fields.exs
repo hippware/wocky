7aa687d3f9aa0d2d595516b98594d99bb7d0567c
Bernard Duggan <bernard@hippware.com>
2017-05-22 10:22:24 +1000

    Change large text fields from BINARY to TEXT type

---
+++ apps/wocky/priv/repo/migrations/20170522001310_use_text_fields.exs
@@ -0,0 +1,26 @@
+defmodule Wocky.Repo.Migrations.UseTextFields do
+  use Ecto.Migration
+
+  def change do
+    alter table(:bots) do
+      modify :description, :text, null: false
+    end
+
+    alter table(:conversations) do
+      modify :message, :text, null: false
+    end
+
+    alter table(:tros_metadatas) do
+      modify :access, :text, null: false
+    end
+
+    alter table(:traffic_logs) do
+      modify :packet, :text, null: false
+    end
+
+    alter table(:home_stream_items) do
+      modify :stanza, :text, null: false
+    end
+  end
+
+end
