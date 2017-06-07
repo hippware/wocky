a43fbb962b9c7241100d7c65bc19cc2a29f6bbf2
Bernard Duggan <bernard@hippware.com>
2017-06-07 10:39:23 +1000

    Add migration file

---
+++ apps/wocky/priv/repo/migrations/20170606055027_remove_unix_timestamps.exs
@@ -0,0 +1,15 @@
+defmodule Wocky.Repo.Migrations.RemoveUnixTimestamps do
+  use Ecto.Migration
+
+  def change do
+    alter table(:tokens) do
+      remove :expires_at
+      add :expires_at, :utc_datetime, null: false, default: "1970-01-01 00:00:00"
+    end
+
+    alter table(:bots) do
+      remove :follow_me_expiry
+      add :follow_me_expiry, :utc_datetime
+    end
+  end
+end
