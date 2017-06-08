f691a8f6b1634bf6410e6681d872eb8009053e93
Bernard Duggan <bernard@hippware.com>
2017-06-08 16:08:05 +1000

    Remove null values before migration

--- apps/wocky/priv/repo/migrations/20170606023418_remove_bot_nulls.exs
+++ apps/wocky/priv/repo/migrations/20170606023418_remove_bot_nulls.exs
@@ -1,7 +1,34 @@
 defmodule Wocky.Repo.Migrations.RemoveBotNulls do
   use Ecto.Migration
 
+  import Ecto.Query
+
+  alias Wocky.Repo
+
   def change do
+
+    from(b in "bots",
+      update: [set: [title: ""]],
+      where: is_nil b.title)
+    |> Repo.update_all([])
+
+    from(b in "bots",
+      update: [set: [address: ""]],
+      where: is_nil b.address)
+    |> Repo.update_all([])
+
+    from(b in "bots",
+      update: [set: [type: ""]],
+      where: is_nil b.type)
+    |> Repo.update_all([])
+
+    from(b in "bots",
+      update: [set: [description: ""]],
+      where: is_nil b.description)
+    |> Repo.update_all([])
+
+    flush()
+
     alter table(:bots) do
       modify :title,       :string, null: false, default: ""
       modify :address,     :string, null: false, default: ""
@@ -9,4 +36,5 @@ defmodule Wocky.Repo.Migrations.RemoveBotNulls do
       modify :description, :text,   null: false, default: ""
     end
   end
+
 end
