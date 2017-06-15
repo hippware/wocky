b5aaed99f85f99ce5193f505540bfb72dbe6b3aa
Phil Toland <phil@hippware.com>
2017-06-15 17:15:06 -0500

    Use streams when reindexing the database

--- apps/wocky/lib/wocky/index.ex
+++ apps/wocky/lib/wocky/index.ex
@@ -94,26 +94,12 @@ defmodule Wocky.Index do
   end
 
   def handle_call({:reindex, :users}, _, state) do
-    index = state.indexes[:user]
-
-    User
-    |> Repo.all
-    |> Enum.each(fn %User{id: id} = user ->
-      update_index(index, id, user, @fields[:user], state.backend)
-    end)
-
+    do_reindex(:user, User, state)
     {:reply, :ok, state}
   end
 
   def handle_call({:reindex, :bots}, _, state) do
-    index = state.indexes[:bot]
-
-    Bot
-    |> Repo.all
-    |> Enum.each(fn %Bot{id: id} = bot ->
-      update_index(index, id, bot, @fields[:bot], state.backend)
-    end)
-
+    do_reindex(:bot, Bot, state)
     {:reply, :ok, state}
   end
 
@@ -121,9 +107,8 @@ defmodule Wocky.Index do
     {:reply, {:error, :unknown_index}, state}
   end
 
-  def handle_call({:update, obj_t, id, fields}, _, state) do
-    index = state.indexes[obj_t]
-    update_index(index, id, fields, @fields[obj_t], state.backend)
+  def handle_call({:update, obj_t, id, data}, _, state) do
+    update_object(obj_t, id, data, state)
     {:reply, :ok, state}
   end
 
@@ -144,15 +129,33 @@ defmodule Wocky.Index do
   # ===================================================================
   # Helpers
 
-  defp update_index(index, id, data, fields, backend) do
+  defp do_reindex(obj_t, query, state) do
+    Repo.transaction fn ->
+      query
+      |> Repo.stream
+      |> Stream.each(&update_object(obj_t, &1, state))
+      |> Stream.run
+    end
+  end
+
+  defp update_object(obj_t, %{id: id} = data, state) do
+    update_object(obj_t, id, data, state)
+  end
+
+  defp update_object(obj_t, id, data, state) do
+    index = state.indexes[obj_t]
+    update_object(index, id, data, @fields[obj_t], state.backend)
+  end
+
+  defp update_object(index, id, data, fields, backend) do
     data
     |> Map.take(fields)
     |> Enum.reject(fn {_k, v} -> v == nil end)
     |> Enum.into(%{}, fn {k, v} -> {to_string(k), v} end)
-    |> do_update_index(id, index, backend)
+    |> do_update_object(id, index, backend)
   end
 
-  defp do_update_index(object, id, index, backend) do
+  defp do_update_object(object, id, index, backend) do
     :ok = Logger.debug("Updating #{index} index with object #{inspect(object)}")
     if map_size(object) < 1 do
       {:ok, :no_changes}
