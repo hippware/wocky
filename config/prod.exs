ea7d6fdc7e6e07e6f457338eaa2fd262d415026f
Phil Toland <phil@hippware.com>
2017-08-28 18:05:56 -0500

    Use the FQDN for clustering the wocky containers

--- config/prod.exs
+++ config/prod.exs
@@ -4,6 +4,6 @@ use Mix.Config
 config :logger, level: :info
 
 config :peerage, via: Peerage.Via.Dns,
-  dns_name: "wocky-private",
+  dns_name: "wocky-private.${KUBE_POD_NS}.svc.cluster.local",
   app_name: "wocky",
   interval: 5
