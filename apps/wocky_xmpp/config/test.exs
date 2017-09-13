9547ea9d7ebbf8d40cfcf4795155ac810eed2b87
Phil Toland <phil@hippware.com>
2017-09-13 16:25:05 -0500

    Fix Lager config when running tests

--- apps/wocky_xmpp/config/test.exs
+++ apps/wocky_xmpp/config/test.exs
@@ -11,22 +11,17 @@ config :logger,
 
 config :lager,
   handlers: [
-    {:lager_console_backend, [
-      :critical,
-      {:lager_default_formatter, [:time, :color, ' [', :severity, '] ', :message, '\e[0m\r\n']}
-    ]},
-    {:lager_file_backend, [
-      {:file, 'wocky.log'},
-      {:level, :warning},
-      {:size, 2097152},
-      {:date, '$D0'},
-      {:count, 5}
-    ]},
-    {:lager_file_backend, [
-      {:file, 'debug.log'},
-      {:level, :debug},
-      {:size, 2097152},
-      {:date, '$D0'},
-      {:count, 5}
-    ]}
+    lager_console_backend: [
+      level: :critical,
+      formatter: :lager_default_formatter,
+      formatter_config: [:time, :color, ' [', :severity, '] ', :message, '\e[0m\r\n']
+    ],
+    lager_file_backend: [
+      file: 'wocky.log',
+      level: :info
+    ],
+    lager_file_backend: [
+      file: 'debug.log',
+      level: :debug
+    ]
   ]
