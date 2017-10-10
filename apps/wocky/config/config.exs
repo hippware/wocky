447b984aedb2303771be22fd3dcc6cca83de8e71
Phil Toland <phil@hippware.com>
2017-10-10 16:12:46 -0500

    Run the bot report as a Distillery custom command

--- apps/wocky/config/config.exs
+++ apps/wocky/config/config.exs
@@ -4,21 +4,36 @@ use Mix.Config
 
 config :wocky,
   ecto_repos: [Wocky.Repo],
+
+  # "Follow me" and location processing
   enable_bot_event_notifications: true,
   enable_follow_me_updates: true,
   async_location_processing: false,
+
+  # TROS file storage in S3
   tros_backend: Wocky.TROS.TestStore,
   tros_s3_bucket:        {:system, "WOCKY_TROS_S3_BUCKET", "wocky-tros-test"},
   tros_s3_region:        {:system, "WOCKY_S3_REGION"},
   tros_s3_access_key_id: {:system, "WOCKY_S3_ACCESS_KEY_ID"},
   tros_s3_secret_key:    {:system, "WOCKY_S3_SECRET_KEY"},
+
+  # FTS indexing (Algolia)
   indexing_system:       {:system, "WOCKY_INDEXING_SYSTEM", "test"},
   user_index_name:       {:system, "WOCKY_USER_INDEX_NAME"},
   bot_index_name:        {:system, "WOCKY_BOT_INDEX_NAME"},
+
+  # Bot reports
   enable_bot_report:     {:system, :boolean, "WOCKY_ENABLE_BOT_REPORT", false},
+  bot_report_channel:    {:system, :string,  "WOCKY_BOT_REPORT_CHANNEL", "wocky-reports"},
+  bot_report_days:       {:system, :integer, "WOCKY_BOT_REPORT_DAYS", 7},
+
+  # Push notifications
   enable_push_notifications: {:system, :boolean, "WOCKY_ENABLE_PUSH", false},
+
+  # Firebase
   firebase_project_id:   "my-project-1480497595993",
 
+  # Welcome email
   welcome_email_template: "official_tr_welcome_email",
   welcome_email_from:     {"tinyrobot support", "support@tinyrobot.com"},
   welcome_email_subject:  "Welcome to tinyrobot!",
