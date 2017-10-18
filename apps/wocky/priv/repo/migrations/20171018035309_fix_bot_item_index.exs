42d07177c10957ed6893b9c540b0f1d8dd6c9e99
Bernard Duggan <bernard@hippware.com>
2017-10-19 09:24:34 +1100

    Revert PK changes

--- apps/wocky/priv/repo/migrations/20171018035309_fix_bot_item_index.exs
+++ apps/wocky/priv/repo/migrations/20171018035309_fix_bot_item_index.exs
@@ -2,17 +2,7 @@ defmodule Wocky.Repo.Migrations.FixBotItemIndex do
   use Ecto.Migration
   @disable_ddl_transaction true
 
-  def up do
-    execute "ALTER TABLE bot_items DROP CONSTRAINT bot_items_pkey"
-    execute "ALTER TABLE bot_items ADD PRIMARY KEY (id)"
-
+  def change do
     create index(:bot_items, [:bot_id], [concurrently: true])
-  end
-
-  def down do
-    execute "ALTER TABLE bot_items DROP CONSTRAINT bot_items_pkey"
-    execute "ALTER TABLE bot_items ADD PRIMARY KEY (id, bot_id)"
-
-    drop index(:bot_items, [:bot_id], [concurrently: true])
   end
 end
