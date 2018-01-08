c98ba596373e543b5dee987f0f3efc8e79c0fa52
Bernard Duggan <bernard@hippware.com>
2018-01-09 10:29:37 +1100

    Typo fix

--- apps/wocky_db_watcher/mix.exs
+++ apps/wocky_db_watcher/mix.exs
@@ -9,7 +9,6 @@ defmodule WockyDbWatcher.Mixfile do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       elvis_config: [%{src_dirs: [], rules: []}],
