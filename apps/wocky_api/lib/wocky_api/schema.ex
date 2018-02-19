d7dd9e1e46bd257f96215a30433cf7d1b3043e9c
Phil Toland <phil@hippware.com>
2018-02-19 16:33:28 -0700

    Update the GraphQL data model
    Add mutation input objects, clientMutationId, and connection objects for paginating collections.

--- apps/wocky_api/lib/wocky_api/schema.ex
+++ apps/wocky_api/lib/wocky_api/schema.ex
@@ -5,65 +5,171 @@ defmodule WockyAPI.Schema do
 
   alias WockyAPI.UserResolver
 
-  object :user do
-    field :profile, non_null(:profile)
-    field :home_stream, non_null(list_of(non_null(:home_stream_item)))
-    field :contacts, non_null(list_of(non_null(:contact))) do
-      arg :relationship, :relationship
+  object :current_user do
+    field :profile, non_null(:profile) do
+      resolve &UserResolver.get_profile/3
     end
-    field :conversations, non_null(list_of(non_null(:conversation)))
-    field :bots, non_null(list_of(non_null(:bot)))
-    field :shares, non_null(list_of(non_null(:bot)))
-    field :subscriptions, non_null(list_of(non_null(:bot)))
-    field :visible_users, non_null(list_of(non_null(:profile)))
+
+    field :contacts_connection, :user_contacts_connection do
+      arg :first, :integer
+      arg :after, :string
+      arg :last, :integer
+      arg :before, :string
+      arg :relationship, :user_contact_relationship
+
+      resolve &UserResolver.get_contacts/3
+    end
+
+    field :home_stream_connection, :user_home_stream_connection do
+      arg :first, :integer
+      arg :after, :string
+      arg :last, :integer
+      arg :before, :string
+
+      resolve &UserResolver.get_home_stream/3
+    end
+
+    field :conversations_connection, :user_conversations_connection do
+      arg :first, :integer
+      arg :after, :string
+      arg :last, :integer
+      arg :before, :string
+
+      resolve &UserResolver.get_conversations/3
+    end
+
+    field :bots_connection, :user_bots_connection do
+      arg :first, :integer
+      arg :after, :string
+      arg :last, :integer
+      arg :before, :string
+      arg :relationship, :user_bot_relationship
+
+      resolve &UserResolver.get_bots/3
+    end
   end
 
   object :profile do
-    field :id, non_null(:string)
-    field :username, non_null(:string)
-    field :handle, non_null(:string)
-    field :avatar, non_null(:string)
-    field :first_name, non_null(:string)
-    field :last_name, non_null(:string)
+    field :id, non_null(:id)
+    field :server, non_null(:string)
+    field :external_id, :string
+    field :phone_number, :string
+    field :email, :string
+    field :handle, :string
+    field :avatar, :string
+    field :first_name, :string
+    field :last_name, :string
     field :tagline, :string
     field :roles, non_null(list_of(non_null(:string)))
+    field :bot_count, :integer
+    field :follower_count, :integer
+    field :following_count, :integer
   end
 
-  object :home_stream_item do
-    field :id, non_null(:string)
+  object :page_info do
+    field :has_next_page, non_null(:boolean)
+    field :has_previous_page, non_null(:boolean)
   end
 
-  enum :relationship do
+  enum :user_contact_relationship do
     value :following
     value :follower
     value :friend
   end
 
-  object :contact do
-    field :relationship, non_null(:relationship)
-    field :profile, non_null(:profile)
+  object :user_contacts_connection do
+    field :page_info, non_null(:page_info)
+    field :edges, list_of(:user_contacts_edge)
+  end
+
+  object :user_contacts_edge do
+    field :cursor, non_null(:string)
+    field :relationship, non_null(:user_contact_relationship)
+    field :node, non_null(:profile)
+  end
+
+  object :user_home_stream_connection do
+    field :page_info, non_null(:page_info)
+    field :edges, list_of(:user_home_stream_edge)
+  end
+
+  object :user_home_stream_edge do
+    field :cursor, non_null(:string)
+    field :node, non_null(:home_stream_item)
+  end
+
+  object :home_stream_item do
+    field :id, non_null(:id)
+    # ...
+  end
+
+  object :user_conversations_connection do
+    field :page_info, non_null(:page_info)
+    field :edges, list_of(:user_conversations_edge)
+  end
+
+  object :user_conversations_edge do
+    field :cursor, non_null(:string)
+    field :node, non_null(:conversation)
   end
 
   object :conversation do
-    field :id, non_null(:string)
+    field :id, non_null(:id)
+    # ...
+  end
+
+  enum :user_bot_relationship do
+    value :owned
+    value :shared
+    value :subscribed
+  end
+
+  object :user_bots_connection do
+    field :page_info, non_null(:page_info)
+    field :edges, list_of(:user_bots_edge)
+  end
+
+  object :user_bots_edge do
+    field :cursor, non_null(:string)
+    field :relationship, non_null(:user_bot_relationship)
+    field :node, non_null(:bot)
   end
 
   object :bot do
-    field :id, non_null(:string)
+    field :id, non_null(:id)
+    # ...
   end
 
   query do
-    field :current_user, non_null(:user) do
-      resolve &UserResolver.current_user/3
+    field :current_user, non_null(:current_user)
+
+    field :users, non_null(:profile) do
+      arg :id, non_null(:string)
+
+      resolve &UserResolver.get_user/3
     end
   end
 
-  mutation do
-    field :set_handle, :user do
-      arg :id, non_null(:string)
-      arg :handle, non_null(:string)
+  input_object :update_profile_input do
+    field :client_mutation_id, :string
+    field :handle, :string
+    field :avatar, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :tagline, :string
+  end
 
-      resolve &UserResolver.set_handle/3
+  object :update_profile_payload do
+    field :client_mutation_id, :string
+    field :profile, :profile
+  end
+
+  mutation do
+    field :update_profile, :update_profile_payload do
+      arg :input, non_null(:update_profile_input)
+
+      resolve &UserResolver.update_profile/3
     end
   end
 end
--- apps/wocky_api/lib/wocky_api/user_resolver.ex
+++ apps/wocky_api/lib/wocky_api/user_resolver.ex
@@ -4,21 +4,31 @@ defmodule WockyAPI.UserResolver do
   alias Wocky.Repo
   alias Wocky.User
 
-  def all_users(_root, _args, _info) do
-    users = Repo.all(User)
-    {:ok, users}
+  def get_profile(_root, _args, %{context: %{current_user: current_user}}) do
+    {:ok, current_user}
   end
 
-  def current_user(_root, _args, _info) do
-  end
-
-  def set_handle(_root, args, _info) do
-    IO.puts inspect(args)
-    user = Repo.get!(User, args[:id])
-    user = Ecto.Changeset.change user, handle: args[:handle]
+  def update_profile(_root, args, %{context: %{current_user: current_user}}) do
+    user = Ecto.Changeset.change current_user, args
     case Repo.update(user) do
       {:ok, user} -> {:ok, user}
       {:error, _} -> {:error, "Could not update handle"}
     end
+  end
+
+  def get_contacts(_root, _args, _info) do
+  end
+
+  def get_home_stream(_root, _args, _info) do
+  end
+
+  def get_conversations(_root, _args, _info) do
+  end
+
+  def get_bots(_root, _args, _info) do
+  end
+
+  def get_user(_root, args, %{context: %{current_user: _current_user}}) do
+    Repo.get(User, args[:id])
   end
 end
