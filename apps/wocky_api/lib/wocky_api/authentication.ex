0faa25e7bdd3336055220a1238269666ab835a72
Phil Toland <phil@hippware.com>
2018-02-20 15:29:05 -0700

    Oops. Fix a regression in the authentication plug

--- apps/wocky_api/lib/wocky_api/authentication.ex
+++ apps/wocky_api/lib/wocky_api/authentication.ex
@@ -25,9 +25,9 @@ defmodule WockyAPI.Authentication do
 
   def check_owner_access(conn, _opts \\ []) do
     path_user = conn.path_params["user_id"]
-    current_user = Map.get(conn.assigns, :current_user)
+    user = Map.get(conn.assigns, :current_user)
 
-    if is_nil(path_user) || current_user.id == path_user do
+    if is_nil(path_user) || (!is_nil(user) && user.id == path_user) do
       conn
     else
       conn |> send_resp(:forbidden, "") |> halt()
