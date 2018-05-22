5b246df580b30f8f7f9a47dceb433b06551ac910
Bernard Duggan <bernard@hippware.com>
2018-05-22 15:03:57 +1000

    Add schema documentation

--- apps/wocky_api/lib/wocky_api/schema/media_types.ex
+++ apps/wocky_api/lib/wocky_api/schema/media_types.ex
@@ -24,24 +24,43 @@ defmodule WockyAPI.Schema.MediaTypes do
   end
 
   input_object :media_upload_params do
+    @desc "Name of the file being uploaded"
     field :filename, non_null(:string)
+
+    @desc "Size of the file being uploaded in bytes"
     field :size, non_null(:integer)
+
+    @desc "MIME type of the file being uploaded (eg image/jpeg)"
     field :mime_type, non_null(:string)
+
+    @desc "Access string for the file being uploaded"
     field :access, :string
   end
 
   payload_object(:media_upload_payload, :media_upload_result)
 
   object :request_header do
+    @desc "HTTP Header name"
     field :name, non_null(:string)
+
+    @desc "HTTP Header value"
     field :value, non_null(:string)
   end
 
   object :media_upload_result do
+    @desc "ID of the file to be uploaded"
     field :id, non_null(:uuid)
+
+    @desc "Destination URL to which the file should be uploaded"
     field :upload_url, non_null(:string)
+
+    @desc "HTTP method to use for upload request"
     field :method, non_null(:string)
+
+    @desc "HTTP headers to include in upload request"
     field :headers, non_null(list_of(:request_header))
+
+    @desc "TROS URL by which the file will be referred to in other objects"
     field :reference_url, non_null(:string)
   end
 
