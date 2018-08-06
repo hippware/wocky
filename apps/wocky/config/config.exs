b110933ec1fd4b5b0e14763d8fa9c16439e58abd
Phil Toland <phil@hippware.com>
2018-08-06 11:24:57 -0600

    Drop max_accuracy_threshold to 30 meters
    The more aggressive value may be better for initial fields tests.

--- apps/wocky/config/config.exs
+++ apps/wocky/config/config.exs
@@ -52,7 +52,7 @@ config :wocky, Wocky.User.GeoFence,
   visit_timeout_enabled:
     {:system, :boolean, "WOCKY_VISIT_TIMEOUT_ENABLED", true},
   max_accuracy_threshold:
-    {:system, :integer, "WOCKY_GEOFENCE_MAX_ACCURACY_THRESHOLD", 50},
+    {:system, :integer, "WOCKY_GEOFENCE_MAX_ACCURACY_THRESHOLD", 30},
   max_slow_speed: {:system, :integer, "WOCKY_GEOFENCE_MAX_SLOW_SPEED", 2},
   max_exit_distance:
     {:system, :integer, "WOCKY_GEOFENCE_MAX_EXIT_DISTANCE", 200}
