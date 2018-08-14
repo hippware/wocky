5970df9b65da93eed1620235fdfac0de237efba9
Phil Toland <phil@hippware.com>
2018-08-14 17:16:20 -0600

    Fix the bot event timestamp migration

--- apps/wocky/priv/repo/migrations/20180806213418_bot_event_occurred_at.exs
+++ apps/wocky/priv/repo/migrations/20180806213418_bot_event_occurred_at.exs
@@ -1,21 +1,7 @@
 defmodule Wocky.Repo.Migrations.BotEventOccurredAt do
   use Ecto.Migration
 
-  def up do
-    alter table(:user_bot_events) do
-      add :occurred_at, :timestamptz, null: false
-      remove :updated_at
-    end
-
-    execute "update user_bot_events set occurred_at = created_at;"
-  end
-
-  def down do
-    alter table(:user_bot_events) do
-      add :updated_at, :timestamptz, null: false
-      remove :occurred_at
-    end
-
-    execute "update user_bot_events set updated_at = created_at;"
+  def change do
+    rename table(:user_bot_events), :updated_at, to: :occurred_at
   end
 end
