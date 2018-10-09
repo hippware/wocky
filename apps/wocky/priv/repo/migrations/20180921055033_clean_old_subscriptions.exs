876ccdaceff15beaa62b444d4be1355416007688
Phil Toland <phil@hippware.com>
2018-10-09 13:21:41 -0600

    We don't want to delete old subscriptions

--- apps/wocky/priv/repo/migrations/20180921055033_clean_old_subscriptions.exs
+++
@@ -1,8 +0,0 @@
-defmodule Wocky.Repo.Migrations.CleanOldSubscriptions do
-  use Wocky.Repo.Migration
-
-  def up do
-    execute "DELETE FROM bot_subscriptions"
-    execute "DELETE FROM bot_invitations"
-  end
-end
