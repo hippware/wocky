2599af96c5b4ec40a755b37478297f9345e34833
Phil Toland <phil@hippware.com>
2018-10-26 15:20:21 -0600

    Add checkpoint migration and remove bot_shares

---
+++ apps/wocky/priv/repo/migrations/20181026190739_checkpoint.exs
@@ -0,0 +1,9 @@
+defmodule Wocky.Repo.Migrations.Checkpoint do
+  use Wocky.Repo.Migration
+
+  def up do
+    # Do nothing for now.
+    # Once this has been run in all environments, this will be replaced
+    # by code to recreate the current schema from scratch.
+  end
+end
