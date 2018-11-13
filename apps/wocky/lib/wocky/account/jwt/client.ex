1d1711de97a82bf107e3899960cb63713aac0872
Phil Toland <phil@hippware.com>
2018-11-13 16:19:51 -0700

    Refactor JWT claims verification

--- apps/wocky/lib/wocky/account/jwt/client.ex
+++ apps/wocky/lib/wocky/account/jwt/client.ex
@@ -3,12 +3,9 @@ defmodule Wocky.Account.JWT.Client do
   Validates the client-generated JWT used to wrap OAuth 2 tokens.
   See https://github.com/hippware/tr-wiki/wiki/Authentication-proposal
   """
-  @audience "Wocky"
-
   use Guardian,
     otp_app: :wocky,
     issuer: "TinyRobot/0.0.0 (Wocky)",
-    audience: @audience,
     secret_key:
       "0xszZmLxKWdYjvjXOxchnV+ttjVYkU1ieymigubkJZ9dqjnl7WPYLYqLhvC10TaH",
     token_verify_module: Wocky.Account.JWT.Verify
@@ -16,6 +13,9 @@ defmodule Wocky.Account.JWT.Client do
   alias Wocky.Account.JWT.Firebase
   alias Wocky.Account.Register
   alias Wocky.User
+
+  @audience "Wocky"
+  @agent_rx ~r/TinyRobot\/(\d+\.\d+\.\d+)(?: \((.*)\))?/
 
   def subject_for_token(%User{} = user, _claims) do
     {:ok, Register.get_external_id(user, "bypass")}
@@ -62,4 +62,66 @@ defmodule Wocky.Account.JWT.Client do
   end
 
   def build_claims(claims, _resource, _opts), do: {:ok, claims}
+
+  def verify_claims(claims, _opts) do
+    with :ok <- verify_aud(claims["aud"]),
+         :ok <- verify_typ(claims["typ"]),
+         :ok <- verify_iss(claims["iss"]) do
+      {:ok, claims}
+    end
+  end
+
+  defp verify_aud(aud) do
+    if is_nil(aud) || aud == @audience do
+      :ok
+    else
+      {:error, :invalid_audience}
+    end
+  end
+
+  defp verify_typ(typ) do
+    if Enum.member?(["bypass", "firebase"], typ) do
+      :ok
+    else
+      {:error, :unknown_token_type}
+    end
+  end
+
+  defp verify_iss(iss) do
+    if client_supported?(iss) do
+      :ok
+    else
+      {:error, :invalid_issuer}
+    end
+  end
+
+  defp client_supported?(nil), do: false
+
+  defp client_supported?(agent_str) do
+    case parse_agent(agent_str) do
+      {:ok, version, attrs} -> client_supported?(version, attrs)
+      {:error, _} -> false
+    end
+  end
+
+  defp parse_agent(agent_str) do
+    case Regex.run(@agent_rx, agent_str) do
+      nil -> {:error, :unknown_client}
+      [_, version] -> {:ok, version, []}
+      [_, version, attrs] -> {:ok, version, parse_attrs(attrs)}
+    end
+  end
+
+  defp parse_attrs(""), do: []
+
+  defp parse_attrs(attrs) do
+    attrs
+    |> String.split(";", trim: true)
+    |> Enum.each(&String.trim/1)
+  end
+
+  defp client_supported?(_version, _attrs) do
+    # Always return true for now
+    true
+  end
 end
--- apps/wocky/test/wocky/account/jwt/verify_test.exs
+++ apps/wocky/test/wocky/account/jwt/verify_test.exs
@@ -9,30 +9,9 @@ defmodule Wocky.Account.JWT.VerifyTest do
     {:ok, claims} == Verify.verify_claim(ClientJWT, name, claims, [])
   end
 
-  test "iss validation" do
-    assert verify_claim("iss", "TinyRobot/1.0.0")
-    assert verify_claim("iss", "TinyRobot/1.0.0 (Testing)")
-    assert verify_claim("iss", "TinyRobot/1.0.0 (Wocky; Testing)")
-    refute verify_claim("iss", "Foo")
-    refute verify_claim("iss", nil)
-  end
-
-  test "aud validation" do
-    assert verify_claim("aud", nil)
-    assert verify_claim("aud", "Wocky")
-    refute verify_claim("aud", "Anything Else")
-  end
-
   test "iat validation" do
     assert verify_claim("iat", nil)
     assert verify_claim("iat", Guardian.timestamp())
     refute verify_claim("iat", Guardian.timestamp() + 100)
-  end
-
-  test "typ validation" do
-    assert verify_claim("typ", "bypass")
-    assert verify_claim("typ", "firebase")
-    refute verify_claim("typ", "foo")
-    refute verify_claim("typ", nil)
   end
 end
