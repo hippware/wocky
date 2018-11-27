47f3657b58d1c135ea068800dfb9342ab1bbc216
Phil Toland <phil@hippware.com>
2018-11-27 14:17:00 -0700

    Rewrite migration task for Ecto 3

--- rel/commands/predeploy.sh
+++ rel/commands/predeploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 release_ctl eval 'Wocky.Tasks.Notify.start()'
-release_ctl eval 'Wocky.Tasks.Migrate.run()'
+release_ctl eval --mfa "Wocky.Tasks.Migrate.migrate/1" --argv -- "$@"
