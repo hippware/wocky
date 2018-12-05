a4044524ac36a2e9df2fee38a32c409dab989f96
Bernard Duggan <bernard@hippware.com>
2018-12-05 16:44:13 +1100

    Move search tests to async test module

--- apps/wocky/test/wocky/user/user_async_test.exs
+++ apps/wocky/test/wocky/user/user_async_test.exs
@@ -3,8 +3,18 @@ defmodule Wocky.User.UserAsyncTest do
 
   import Mock
 
+  alias Wocky.{Block, User}
   alias Wocky.Repo.Factory
-  alias Wocky.User
+
+  setup do
+    user = Factory.insert(:user, device: "testing", first_name: "first", last_name: "last", handle: "handle")
+
+    {:ok,
+     user: user,
+     id: user.id,
+     external_id: user.external_id,
+     phone_number: user.phone_number}
+  end
 
   describe "user deletion" do
     setup_with_mocks([
@@ -25,6 +35,75 @@ defmodule Wocky.User.UserAsyncTest do
       assert User.delete(user.id) == :ok
 
       refute called FirebaseAdminEx.Auth.delete_user(user.external_id)
+    end
+  end
+
+  describe "search_by_name/3" do
+    setup do
+      users =
+        [
+          {"Alice", "Sanders", "Xena"},
+          {"Alison", "Smith", "Yaniv"},
+          {"Bob", "Jones", "Zena"},
+          {"acéñtîâ", "CAPITAL", "1345"}
+        ]
+        |> Enum.map(fn {f, l, h} ->
+          Factory.insert(:user, first_name: f, last_name: l, handle: h)
+        end)
+
+      {:ok, users: users}
+    end
+
+    test "should return all users with the search prefix in either name", ctx do
+      assert User.search_by_name("a", ctx.id, 50) |> length() == 3
+      assert User.search_by_name("b", ctx.id, 50) |> length() == 1
+      assert User.search_by_name("s", ctx.id, 50) |> length() == 2
+      assert User.search_by_name("smi", ctx.id, 50) |> length() == 1
+      assert User.search_by_name("q", ctx.id, 50) |> length() == 0
+      assert User.search_by_name("z", ctx.id, 50) |> length() == 1
+      assert User.search_by_name("13", ctx.id, 50) |> length() == 1
+    end
+
+    test "should ignore accents in both search and data", ctx do
+      assert User.search_by_name("acent", ctx.id, 50) |> length() == 1
+      assert User.search_by_name("â", ctx.id, 50) |> length() == 3
+    end
+
+    test "should ignore capitalisation in both search and data", ctx do
+      assert User.search_by_name("A", ctx.id, 50) |> length() == 3
+      assert User.search_by_name("c", ctx.id, 50) |> length() == 1
+    end
+
+    test "should respect the limit parameter", ctx do
+      assert User.search_by_name("a", ctx.id, 2) |> length() == 2
+    end
+
+    test "should ignore empty search terms and return an empty list", ctx do
+      assert User.search_by_name("", ctx.id, 50) |> length() == 0
+    end
+
+    test "should work on multiple partial terms", ctx do
+      assert User.search_by_name("ali s", ctx.id, 50) |> length() == 2
+      assert User.search_by_name("ali sm", ctx.id, 50) |> length() == 1
+    end
+
+    test "should not choke on punctuation or other unicode weirdness", ctx do
+      assert User.search_by_name("''ali", ctx.id, 50) |> length() == 2
+      assert User.search_by_name("al-s", ctx.id, 50) |> length() == 0
+      assert User.search_by_name("al''i", ctx.id, 50) |> length() == 2
+      assert User.search_by_name("al''i", ctx.id, 50) |> length() == 2
+      assert User.search_by_name("''-al''i", ctx.id, 50) |> length() == 2
+    end
+
+    test "should not return a blocking user", ctx do
+      # Alice Sanders
+      blocking_user = hd(ctx.users)
+      Block.block(blocking_user, ctx.user)
+
+      result = User.search_by_name("a", ctx.id, 50)
+
+      assert length(result) == 2
+      refute Enum.any?(result, fn %{id: id} -> id == blocking_user.id end)
     end
   end
 end
