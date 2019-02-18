2d6a505df4ec586097f17aa00b78fd1b12b591ee
Bernard Duggan <bernard@hippware.com>
2019-02-18 17:05:08 +1100

    Use secure redis cluster [take 4]

--- apps/wocky/config/config.exs
+++ apps/wocky/config/config.exs
@@ -78,6 +78,8 @@ config :wocky,
 config :wocky, :redis,
   host: {:system, :string, "REDIS_HOST", "localhost"},
   port: {:system, :integer, "REDIS_PORT", 6379},
+  ssl: {:system, :boolean, "REDIS_SSL", false},
+  password: {:system, :integer, "REDIS_PASSWORD", nil},
   db: {:system, :integer, "REDIS_DB", 0}
 
 config :wocky, :redlock,
