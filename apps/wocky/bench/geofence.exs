dcd069d7532b93cbbe801f650ece1c50478fa1fc
Phil Toland <phil@hippware.com>
2019-05-10 15:07:55 -0600

    Update the geofence benchmark
    Use cached subscriptions and events so that we are measuring the raw throughput of the algorithm.

--- apps/wocky/bench/geofence.exs
+++ apps/wocky/bench/geofence.exs
@@ -6,16 +6,16 @@ bots_10 = Factory.insert(:user)
 bots_100 = Factory.insert(:user)
 bots_1000 = Factory.insert(:user)
 
-Factory.insert_list(10, :bot, user: bots_10)
-Factory.insert_list(100, :bot, user: bots_100)
-Factory.insert_list(1000, :bot, user: bots_1000)
+b10 = Factory.insert_list(10, :bot, user: bots_10)
+b100 = Factory.insert_list(100, :bot, user: bots_100)
+b1000 = Factory.insert_list(1000, :bot, user: bots_1000)
 
 Benchee.run(
   %{
-    no_bots: fn l -> GeoFence.check_for_bot_events(l, no_bots) end,
-    bots_10: fn l -> GeoFence.check_for_bot_events(l, bots_10) end,
-    bots_100: fn l -> GeoFence.check_for_bot_events(l, bots_100) end,
-    bots_1000: fn l -> GeoFence.check_for_bot_events(l, bots_1000) end
+    no_bots: fn l -> GeoFence.check_for_bot_events(l, no_bots, [], %{}) end,
+    bots_10: fn l -> GeoFence.check_for_bot_events(l, bots_10, b10, %{}) end,
+    bots_100: fn l -> GeoFence.check_for_bot_events(l, bots_100, b100, %{}) end,
+    bots_1000: fn l -> GeoFence.check_for_bot_events(l, bots_1000, b1000, %{}) end
   },
   before_each: fn _ -> Factory.build(:location) end
 )
