e96571bd63e17c30517896ac412d0bb49aff5349
Phil Toland <phil@hippware.com>
2019-05-15 16:04:00 -0600

    Formatting

--- apps/wocky/lib/wocky/callbacks/bot.ex
+++ apps/wocky/lib/wocky/callbacks/bot.ex
@@ -23,8 +23,9 @@ defmodule Wocky.Callbacks.Bot do
   end
 
   def handle_update(%Bot{location: new} = bot, %Bot{location: old})
-        when new != old do
+      when new != old do
     bot_with_subs = Repo.preload(bot, [:subscribers])
+
     for user <- bot_with_subs.subscribers do
       LocationHandler.add_subscription(user, bot)
     end
