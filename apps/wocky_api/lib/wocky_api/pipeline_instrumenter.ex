2b3eb8bec354bc91416fb2730c9289c7ccb43d20
Bernard Duggan <bernard@hippware.com>
2019-05-28 16:26:57 +1000

    Aggregate REST request metrics

--- apps/wocky_api/lib/wocky_api/pipeline_instrumenter.ex
+++ apps/wocky_api/lib/wocky_api/pipeline_instrumenter.ex
@@ -2,7 +2,9 @@ defmodule WockyAPI.PipelineInstrumenter do
   @moduledoc false
   use Prometheus.PlugPipelineInstrumenter
 
+  # Replace user, bot, etc UUIDs with a common string to limit the number of
+  # paths we generate metrics for
   def label_value(:request_path, conn) do
-    conn.request_path
+    Regex.replace(~r/[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}/, conn.request_path, "<UUID>")
   end
 end
