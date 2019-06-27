39711d806ed319dc5bba1745ecd991739a5fe596
Phil Toland <phil@hippware.com>
2019-06-27 10:44:41 -0600

    Disable test HTTP server endpoint
    It isn't being used.

--- apps/wocky_api/config/test.exs
+++ apps/wocky_api/config/test.exs
@@ -5,6 +5,6 @@ config :wocky_api,
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :wocky_api, WockyAPI.Endpoint,
-  http: [port: 4001],
-  server: false
+# config :wocky_api, WockyAPI.Endpoint,
+#   http: [port: 4001],
+#   server: false
