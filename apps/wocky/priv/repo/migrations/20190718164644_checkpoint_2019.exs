62d32b59626bc96c6a50da7a4e03d38c4b872631
Phil Toland <phil@hippware.com>
2019-07-18 10:48:26 -0600

    Add a checkpoint migration

---
+++ apps/wocky/priv/repo/migrations/20190718164644_checkpoint_2019.exs
@@ -0,0 +1,10 @@
+defmodule Wocky.Repo.Migrations.Checkpoint2019 do
+  use Wocky.Repo.Migration
+
+  def change do
+    # Do nothing for now.
+    # Once this has been run in all environments, this will be replaced
+    # by code to recreate the current schema from scratch.
+    :ok
+  end
+end
