3e36833c63bf8d21d649e24f4b52ced36f6e5f37
Bernard Duggan <bernard@hippware.com>
2019-08-02 16:11:37 +1000

    Finish secret removal from wocky-testing

--- apps/wocky/config/prod.exs
+++ apps/wocky/config/prod.exs
@@ -7,15 +7,27 @@ config :wocky,
   dynamic_link_backend:
     {:system, :module, "WOCKY_DYN_LINK_BACKEND", Wocky.DynamicLink.Firebase},
   country_code_lookup_method:
-    {:system, :atom, "WOCKY_CC_LOOKUP_METHOD", :twilio}
+    {:system, :atom, "WOCKY_CC_LOOKUP_METHOD", :twilio},
+  goth_private_key: {{:via, VaultAdapter}, "firebase-private-key"},
+  slack_token: {{:via, VaultAdapter}, "slack-token"},
+  client_jwt_signing_key: {{:via, VaultAdapter}, "client-jwt-signing-key"},
+  server_jwt_signing_key: {{:via, VaultAdapter}, "server-jwt-signging-key"}
+
+config :wocky, Wocky.Repo, password: {{:via, VaultAdapter}, "db-password"}
 
 config :wocky, Wocky.Location.GeoFence, visit_timeout_enabled: false
+
+config :wocky, Wocky.Notifier.Email.Mailer,
+  api_key: {{:via, VaultAdapter}, "mandrill-api-key"}
 
 config :wocky, :pigeon,
   apns: [
     cert: {:wocky, "certs/${WOCKY_INST}.crt"},
     key: {:wocky, "certs/${WOCKY_INST}.key"},
     mode: :prod
+  ],
+  fcm: [
+    key: {{:via, VaultAdapter}, "fcm-key"}
   ]
 
 config :dawdle, backend: Dawdle.Backend.SQS
@@ -32,7 +44,7 @@ config :ex_aws,
   access_key_id: :instance_role,
   secret_access_key: :instance_role
 
-config :wocky, :redis, password: {{:via, VaultAdapter}, "redis-password", nil}
+config :wocky, :redis, password: {{:via, VaultAdapter}, "redis-password"}
 
 config :wocky, :redlock,
   servers: [
@@ -40,10 +52,13 @@ config :wocky, :redlock,
       host: {:system, :string, "REDIS_HOST", "localhost"},
       port: {:system, :integer, "REDIS_PORT", 6379},
       ssl: {:system, :boolean, "REDIS_SSL", false},
-      auth: {{:via, VaultAdapter}, "redis-password", nil},
+      auth: {{:via, VaultAdapter}, "redis-password"},
       database: {:system, :integer, "REDIS_DB", 0}
     ]
   ]
 
 config :fun_with_flags, :redis,
   password: {{:via, VaultAdapter}, "redis-password"}
+
+config :honeybadger,
+  api_key: {{:via, VaultAdapter}, "honeybadger-api-key"}
