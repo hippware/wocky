84933d50f1bf06944fa9a4c37482bbe505d6a4c5
Phil Toland <phil@hippware.com>
2019-09-24 16:03:58 -0600

    Disable an apparently unneeded dialyser ignore
    It can be removed after a little research.

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
   ~r|lib/wocky/account/jwt/.*\.ex:.:contract_supertype|,
-  ~r|Function Wocky\..*\.__impl__/1 does not exist.|,
+  # ~r|Function Wocky\..*\.__impl__/1 does not exist.|,
   {"lib/wocky/repo/instrumenter.ex", :unmatched_return, 3},
   {"lib/wocky_api/pipeline_instrumenter.ex", :unmatched_return, 3},
   {"lib/wocky_api/router.ex", :unmatched_return, 35},
