b769cd5a191673a484491435120c1e8a8bdab1d5
Bernard Duggan <bernard@hippware.com>
2019-10-04 15:30:23 +1000

    Fix selection of pid for authenticated websocket

--- apps/wocky_api/lib/wocky_api/resolvers/auth.ex
+++ apps/wocky_api/lib/wocky_api/resolvers/auth.ex
@@ -2,12 +2,10 @@ defmodule WockyAPI.Resolvers.Auth do
   @moduledoc "GraphQL resolver for authentication"
 
   alias Wocky.Account
-  alias WockyAPI.Metrics
 
   def authenticate(_root, %{token: token}, _info) do
     case Account.authenticate(token) do
       {:ok, auth_data} ->
-        Metrics.add_auth_connection(self())
         {:ok, auth_data}
 
       {:error, _} ->
