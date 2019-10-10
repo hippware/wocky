1cab640c2365905f5108b0f6369d7eec12f553a2
Phil Toland <phil@hippware.com>
2019-10-10 16:19:17 -0600

    Send silent pn on shared user connecting

--- apps/wocky/lib/wocky/callbacks/connection.ex
+++ apps/wocky/lib/wocky/callbacks/connection.ex
@@ -4,16 +4,38 @@ defmodule Wocky.Callbacks.Connection do
   use Dawdle.Handler, only: [Wocky.Presence.ConnectionEvent]
 
   alias Wocky.Account.User
+  alias Wocky.Events.LocationRequest
   alias Wocky.Location
+  alias Wocky.Notifier
+
+  @stale_location_seconds 15
 
   def handle_event(%{user: user, status: status}) do
     user
     |> Location.get_location_sharers()
-    |> Enum.each(fn %{user: %User{id: id}} ->
+    |> Enum.each(fn %{user: %User{} = sharer} ->
       case status do
-        :connected -> Location.inc_watcher_count(id)
-        :disconnected -> Location.dec_watcher_count(id)
+        :connected ->
+          Location.inc_watcher_count(sharer)
+          maybe_notify_sharer(sharer)
+
+        :disconnected ->
+          Location.dec_watcher_count(sharer)
       end
     end)
+  end
+
+  defp maybe_notify_sharer(sharer) do
+    loc = Location.get_current_user_location(sharer)
+
+    if stale_location?(loc) do
+      Notifier.notify(%LocationRequest{to: sharer})
+    end
+  end
+
+  defp stale_location?(nil), do: true
+
+  defp stale_location?(%{created_at: created_at}) do
+    Timex.diff(Timex.now(), created_at, :seconds) >= @stale_location_seconds
   end
 end
--- apps/wocky/test/wocky/callbacks/connection_test.exs
+++ apps/wocky/test/wocky/callbacks/connection_test.exs
@@ -4,8 +4,13 @@ defmodule Wocky.Callbacks.ConnectionTest do
   import Eventually
   import Wocky.Presence.TestHelper
 
+  alias Faker.Code
   alias Wocky.Callbacks.Connection, as: Callback
+  alias Wocky.Factory, as: LocationFactory
   alias Wocky.Location
+  alias Wocky.Location.UserLocation.Current
+  alias Wocky.Notifier.Push
+  alias Wocky.Notifier.Push.Backend.Sandbox
   alias Wocky.Repo.Factory
   alias Wocky.Repo.Timestamp
   alias Wocky.Roster
@@ -43,6 +48,78 @@ defmodule Wocky.Callbacks.ConnectionTest do
     disconnect(pid)
 
     assert_eventually(get_watcher_count(ctx.sharer) == 0)
+  end
+
+  describe "push notification" do
+    setup %{sharer: sharer} do
+      pid = Process.whereis(Dawdle.Client)
+      Sandbox.clear_notifications(pid: pid)
+
+      Push.enable(sharer, "testing", Code.isbn13())
+
+      old_value = Sandbox.get_config(:reflect)
+      Sandbox.put_config(:reflect, false)
+
+      on_exit(fn -> Sandbox.put_config(:reflect, old_value) end)
+
+      {:ok, pid: pid}
+    end
+
+    test "should notify on first connection with no current location", ctx do
+      {_, _} = connect(ctx.shared_with)
+
+      assert [n] = Sandbox.wait_notifications(count: 1, pid: ctx.pid)
+      assert Map.get(n.payload, "location-request") == 1
+    end
+
+    test "should not notify on subsequent connections", ctx do
+      {_, _} = connect(ctx.shared_with)
+
+      refute Sandbox.wait_notifications(count: 1, pid: ctx.pid) == []
+
+      Sandbox.clear_notifications(pid: ctx.pid)
+      {_, _} = connect(ctx.shared_with)
+
+      assert Sandbox.list_notifications(pid: ctx.pid) == []
+    end
+
+    test "should not notify on disconnection", ctx do
+      {conn_pid, _} = connect(ctx.shared_with)
+
+      refute Sandbox.wait_notifications(count: 1, pid: ctx.pid) == []
+
+      Sandbox.clear_notifications(pid: ctx.pid)
+      disconnect(conn_pid)
+
+      assert Sandbox.list_notifications(pid: ctx.pid) == []
+    end
+
+    test "should notify with a stale current location", ctx do
+      loc =
+        LocationFactory.build(:user_location,
+          created_at: Timestamp.shift(seconds: -30)
+        )
+
+      Current.set(ctx.sharer, loc)
+
+      {_, _} = connect(ctx.shared_with)
+
+      assert [n] = Sandbox.wait_notifications(count: 1, pid: ctx.pid)
+      assert Map.get(n.payload, "location-request") == 1
+    end
+
+    test "should not notify with a fresh current location", ctx do
+      loc =
+        LocationFactory.build(:user_location,
+          created_at: Timestamp.shift(seconds: -5)
+        )
+
+      Current.set(ctx.sharer, loc)
+
+      {_, _} = connect(ctx.shared_with)
+
+      assert Sandbox.list_notifications(pid: ctx.pid) == []
+    end
   end
 
   defp get_watcher_count(user) do
