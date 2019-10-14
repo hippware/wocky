8ce8cba6c082499219445d50d46d30ed74e33c33
Bernard Duggan <bernard@hippware.com>
2019-10-14 15:50:38 +1100

    Add missing file

---
+++ apps/wocky/test/support/fake_socket.ex
@@ -0,0 +1,35 @@
+defmodule Wocky.Test.FakeSocket do
+  @moduledoc """
+  A process pretending to be a websocket which has registered with the presence
+  manager as belonging to a particular user. Used in testing when we want to
+  test socket registration without the rigmorole of setting up a full websocket.
+  """
+
+  alias Wocky.Presence
+
+  def open(user) do
+    self = self()
+
+    pid =
+      spawn_link(fn ->
+        ref = Process.monitor(self)
+
+        receive do
+          :exit -> :ok
+          {:DOWN, ^ref, :process, _, _} -> :ok
+        end
+      end)
+
+    Presence.register_socket(user, pid)
+    pid
+  end
+
+  def close(pid) do
+    send(pid, :exit)
+    ref = Process.monitor(pid)
+
+    receive do
+      {:DOWN, ^ref, :process, _, _} -> :ok
+    end
+  end
+end
