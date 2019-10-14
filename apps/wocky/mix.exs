7b3ece35b0fada1e5578cd813fb9c815cd39252d
Phil Toland <phil@hippware.com>
2019-10-14 12:14:03 -0600

    Update Pigeon
    Nothing new here, it just contains the iOS 13 patch we pushed upstream earlier.

--- apps/wocky/mix.exs
+++ apps/wocky/mix.exs
@@ -113,8 +113,7 @@ defmodule Wocky.Mixfile do
       {:peerage, "~> 1.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_pubsub_redis, "~> 2.1.5"},
-      # TODO: go back to hex version once changes are merged
-      {:pigeon, github: "hippware/pigeon", branch: "apns-push-type"},
+      {:pigeon, "~> 1.4"},
       {:plug, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
