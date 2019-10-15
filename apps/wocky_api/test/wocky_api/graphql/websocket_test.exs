8baffd945d2aec889cb53f6638914861cc02221c
Bernard Duggan <bernard@hippware.com>
2019-10-15 11:15:22 +1100

    Close all sockets when user is deleted

--- apps/wocky_api/test/wocky_api/graphql/websocket_test.exs
+++ apps/wocky_api/test/wocky_api/graphql/websocket_test.exs
@@ -5,6 +5,7 @@ defmodule WockyAPI.GraphQL.WebsocketTest do
 
   alias AbsintheWebSocket.WebSocket
   alias Ecto.Adapters.SQL.Sandbox, as: SQLSandbox
+  alias Wocky.Account
   alias Wocky.Presence
   alias Wocky.Repo
   alias Wocky.Repo.Factory
@@ -14,22 +15,33 @@ defmodule WockyAPI.GraphQL.WebsocketTest do
 
   setup_all do
     SQLSandbox.mode(Repo, :auto)
+
     {:ok, sup} = start_supervised(Client.supervisor())
     Client.init()
-    WebSocket.set_opt(get_client_socket(sup), :disconnect_sleep, 0)
 
-    {:ok, user: Factory.insert(:user), sup: sup}
+    {:ok, sup: sup}
   end
 
   setup ctx do
+    require_watcher()
+    WockyAPI.Callbacks.User.register()
+
+    user = Factory.insert(:user)
+
+    jwt = APIFactory.get_test_token(user)
+
     await_ready(get_client_socket(ctx.sup))
+    WebSocket.set_opt(get_client_socket(ctx.sup), :disconnect_sleep, 0)
+    WebSocket.set_opt(get_client_socket(ctx.sup), :log_disconnect, false)
 
     on_exit(fn ->
       Supervisor.terminate_child(ctx.sup, WebSocket)
       Supervisor.restart_child(ctx.sup, WebSocket)
+      Dawdle.Client.clear_all_handlers()
+      Wocky.Repo.delete_all(Wocky.Account.User)
     end)
 
-    :ok
+    {:ok, user: user, jwt: jwt}
   end
 
   defp await_ready(socket) do
@@ -45,44 +57,73 @@ defmodule WockyAPI.GraphQL.WebsocketTest do
 
   describe "user socket tracking" do
     test "an unconnected user should have no sockets", ctx do
-      assert_eventually(Presence.get_sockets(ctx.user) == [])
+      assert_eventually(Presence.get_sockets(ctx.user) == [], 500)
     end
 
     test "an authenticated user should have a socket", ctx do
-      jwt = APIFactory.get_test_token(ctx.user)
-      {:ok, _, nil} = Context.authenticate(jwt)
+      {:ok, _, nil} = Context.authenticate(ctx.jwt)
 
-      assert [pid] = Presence.get_sockets(ctx.user)
-      assert is_pid(pid)
+      assert_eventually(length(Presence.get_sockets(ctx.user)) == 1, 500)
     end
 
     test "disconnection should remove the socket from the list", ctx do
-      jwt = APIFactory.get_test_token(ctx.user)
-      {:ok, _, nil} = Context.authenticate(jwt)
+      {:ok, _, nil} = Context.authenticate(ctx.jwt)
 
       WebSocket.close(get_client_socket(ctx.sup))
 
-      assert_eventually(Presence.get_sockets(ctx.user) == [])
+      assert_eventually(Presence.get_sockets(ctx.user) == [], 500)
     end
 
     test "an unconnected user should return false", ctx do
-      refute_eventually(Presence.connected?(ctx.user))
+      refute_eventually(Presence.connected?(ctx.user), 500)
     end
 
     test "an authenticated user shoult return true", ctx do
-      jwt = APIFactory.get_test_token(ctx.user)
-      {:ok, _, nil} = Context.authenticate(jwt)
+      {:ok, _, nil} = Context.authenticate(ctx.jwt)
 
-      assert Presence.connected?(ctx.user)
+      assert_eventually(Presence.connected?(ctx.user), 500)
     end
 
     test "a disconnected user should return false", ctx do
-      jwt = APIFactory.get_test_token(ctx.user)
-      {:ok, _, nil} = Context.authenticate(jwt)
+      {:ok, _, nil} = Context.authenticate(ctx.jwt)
 
       WebSocket.close(get_client_socket(ctx.sup))
-      refute_eventually(Presence.connected?(ctx.user))
+      refute_eventually(Presence.connected?(ctx.user), 500)
     end
+  end
+
+  describe "user deletion" do
+    setup ctx do
+      self = self()
+
+      get_socket()
+      |> AbsintheWebSocket.WebSocket.set_opt(:disconnect_callback, fn ->
+        disconnect_callback(self)
+      end)
+
+      {:ok, _, nil} = Context.authenticate(ctx.jwt)
+
+      :ok
+    end
+
+    test "socket closes on user deletion via API" do
+      {:ok, _, nil} = Context.user_delete()
+
+      assert_receive :disconnected, 500
+    end
+
+    test "socket closes on user deletion via DB", ctx do
+      Account.delete(ctx.user.id)
+
+      assert_receive :disconnected, 500
+    end
+  end
+
+  def disconnect_callback(target), do: send(target, :disconnected)
+
+  defp get_socket do
+    query_server_name = Module.concat([Client.mod(), Caller, QueryServer])
+    :sys.get_state(query_server_name).socket
   end
 
   defp get_client_socket(sup) do
