6b9c55b7d0ac29a574978aad317983f9e5fef0ca
Phil Toland <phil@hippware.com>
2019-11-18 16:26:59 -0700

    Update dialyzer error ignores

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -9,5 +9,5 @@
   # Logger.info ignores:
   {"lib/wocky/repo/cleaner.ex", :unmatched_return},
   {"lib/wocky/application.ex", :unmatched_return},
-  {"lib/wocky/sms/sandbox.ex", :unmatched_return, 11}
+  {"lib/wocky/sms/sandbox.ex", :unmatched_return, 12}
 ]
