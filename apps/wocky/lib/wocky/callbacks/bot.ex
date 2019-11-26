582198444330f17588a38140cfb913dc7dccac51
Phil Toland <phil@hippware.com>
2019-11-26 12:15:44 -0700

    Fix the query

--- apps/wocky/lib/wocky/callbacks/bot.ex
+++ apps/wocky/lib/wocky/callbacks/bot.ex
@@ -7,16 +7,16 @@ defmodule Wocky.Callbacks.Bot do
 
   import Ecto.Query
 
+  alias Wocky.Account.User
   alias Wocky.Location
   alias Wocky.POI.Bot
-  alias Wocky.Relation.Subscription
   alias Wocky.Repo
 
   @impl true
   def handle_update(%Bot{location: new} = bot, %Bot{location: old})
       when new != old do
     bot_with_sub_ids =
-      Repo.preload(bot, [:subscribers, from(s in Subscription, select: s.id)])
+      Repo.preload(bot, subscribers: from(u in User, select: u.id))
 
     for user_id <- bot_with_sub_ids.subscribers do
       Location.refresh_bot_subscriptions(user_id)
