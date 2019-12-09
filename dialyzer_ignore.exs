1c10f47e483aed60b5e44f2c8e415c1183c634ac
Bernard Duggan <bernard@hippware.com>
2019-12-09 12:56:54 +1100

    Move to hex organisation for all custom deps

--- dialyzer_ignore.exs
+++ dialyzer_ignore.exs
@@ -1,7 +1,5 @@
 [
-  ~r|lib/wocky/account/jwt/.*\.ex:.:contract_supertype|,
   ~r|lib/common_graphql_client/client.ex:.*|,
-  # ~r|Function Wocky\..*\.__impl__/1 does not exist.|,
   {"lib/wocky/repo/instrumenter.ex", :unmatched_return, 3},
   {"lib/wocky_api/pipeline_instrumenter.ex", :unmatched_return, 3},
   {"lib/wocky_api/router.ex", :unmatched_return, 35},
