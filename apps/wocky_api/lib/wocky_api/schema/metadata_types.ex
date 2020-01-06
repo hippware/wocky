19de5d15c24962cff341b3071ca7b3fdac591100
Bernard Duggan <bernard@hippware.com>
2020-01-06 20:46:52 +1100

    Add GraphQL docs

--- apps/wocky_api/lib/wocky_api/schema/metadata_types.ex
+++ apps/wocky_api/lib/wocky_api/schema/metadata_types.ex
@@ -5,13 +5,20 @@ defmodule WockyAPI.Schema.MetadataTypes do
 
   alias WockyAPI.Resolvers.Metadata
 
+  @desc "A single item of server metadata"
   object :metadatum do
+    @desc "The datum's key"
     field :key, :string
+
+    @desc "The datum's value"
     field :value, :string
+
+    @desc "A human-readable description of the data"
     field :description, :string
   end
 
   object :metadata_queries do
+    @desc "Retrieve the server's current metadata set"
     field :metadata, list_of(non_null(:metadatum)) do
       resolve &Metadata.get/2
     end
