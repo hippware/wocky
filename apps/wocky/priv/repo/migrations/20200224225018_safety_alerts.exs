7c8fbf0521971af6cb0d826f5f78c2d83768f595
Bernard Duggan <bernard@hippware.com>
2020-02-28 19:16:23 +1100

    Add geometries table

--- apps/wocky/priv/repo/migrations/20200224225018_safety_alerts.exs
+++ apps/wocky/priv/repo/migrations/20200224225018_safety_alerts.exs
@@ -17,6 +17,16 @@ defmodule Wocky.Repo.Migrations.SafetyAlerts do
       add :imported, :boolean, null: false, default: true
     end
 
+    create table("safety_alerts_geometries") do
+      add :source, :text, null: false
+      add :source_id, :text, null: false
+      add :geometry, :geometry, null: false
+      add :data, :map, null: false, default: %{}
+      add :created_at, :timestamptz, null: false
+      add :updated_at, :timestamptz, null: false
+    end
+
     create index("safety_alerts", [:source, :source_id], unique: true)
+    create index("safety_alerts_geometries", [:source, :source_id], unique: true)
   end
 end
